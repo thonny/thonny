da002dd306f3a9d4772b0f9080a095bc59c1127a
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2014-11-01 02:56:49 +0200

    more tweaks

--- thonny/__main__.py
+++ thonny/__main__.py
@@ -1,14 +1,11 @@
 import sys
 import os.path
 
-if "" in sys.path:
-    sys.path.remove("") # don't want current directory in path
-
+# This may be executed without parent of thonny src folder being in path
 # Where are we?
 assert(__file__)
 src_dir = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
 sys.path.insert(0, os.path.normpath(os.path.join(src_dir, os.pardir)))
-
 
 from thonny import main
 main.Thonny(src_dir).mainloop()    
--- thonny/backlaunch.py
+++ thonny/backlaunch.py
@@ -13,10 +13,6 @@ import sys
 import logging
 import os.path
 
-
-if "" in sys.path:
-    sys.path.remove("") # don't want current directory in path
-
 # Where are we?
 assert(__file__)
 src_dir = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
