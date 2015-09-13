519f7f1cd744ec714871ddb5bea8430eb0ae884b
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-13 23:55:57 +0300

    Made windows freezing more specific

--- installers/windows/create_installer.bat
+++ installers/windows/create_installer.bat
@@ -4,7 +4,7 @@ REM following two variables are fix for a bug in cx_freeze in Python 3.5
 REM set TCL_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tcl8.6
 REM set TK_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tk8.6
 
-py -3.4 ..\cx_freeze\setup.py build > freezing.log
+C:\python34\python ..\cx_freeze\setup.py build > freezing.log
 
 set /p VERSION=<..\..\thonny\VERSION
 "C:\Program Files (x86)\Inno Setup 5\iscc" /dAppVer=%VERSION% inno_setup.iss > installer_building.log
