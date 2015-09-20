f7bc678782019c1509533afc05692ab2f6245b62
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-20 09:43:21 +0300

    fullscreen example

---
+++ misc/fullscreen.py
@@ -0,0 +1,31 @@
+import sys
+if sys.version_info[0] == 2:  # Just checking your Python version to import Tkinter properly.
+    from Tkinter import *
+else:
+    from tkinter import *
+
+
+class Fullscreen_Window:
+
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.wm_state("zoomed")  # This just maximizes it so we can see the window. It's nothing to do with fullscreen.
+        self.frame = Frame(self.tk)
+        self.frame.pack()
+        self.state = False
+        self.tk.bind("<F11>", self.toggle_fullscreen)
+        self.tk.bind("<Escape>", self.end_fullscreen)
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state  # Just toggling the boolean
+        self.tk.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.tk.attributes("-fullscreen", False)
+        return "break"
+
+if __name__ == '__main__':
+    w = Fullscreen_Window()
+    w.tk.mainloop()
