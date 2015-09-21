8e1905900c4443ba11a12197a40764f2420d1e4d
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-21 17:17:42 +0300

    Turns out dpiAware has to be true

--- installers/windows/EnableDPIAwareness.bat
+++ installers/windows/EnableDPIAwareness.bat
@@ -1,7 +1,7 @@
 @SET PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86
 
 mt.exe -inputresource:%1;#1 -out:extracted.manifest
-mt.exe -manifest DisableDPIAwareness.manifest extracted.manifest -out:merged.manifest
+mt.exe -manifest EnableDPIAwareness.manifest extracted.manifest -out:merged.manifest
 mt.exe -outputresource:%1;#1 -manifest merged.manifest
 
 del extracted.manifest
