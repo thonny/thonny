38d00cdf3a4daa2408e206bd01dd9bd2f0848bdf
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-21 13:07:48 +0300

    source folder now comes from command line to inno setup + small tweaks in wording

---
+++ installers/windows/DisableDPIAwareness.bat
@@ -0,0 +1,8 @@
+@SET PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86
+
+mt.exe -inputresource:%1;#1 -out:extracted.manifest
+mt.exe -manifest DisableDPIAwareness.manifest extracted.manifest -out:merged.manifest
+mt.exe -outputresource:%1;#1 -manifest merged.manifest
+
+del extracted.manifest
+del merged.manifest
--- installers/windows/create_installer.bat
+++ installers/windows/create_installer.bat
@@ -1,11 +1,21 @@
+set FREEZE_TARGET=cx_build\exe.win32-3.4
+
+@echo ............... CLEANING .................
 rmdir build /s /q
+
 
 REM following two variables are fix for a bug in cx_freeze in Python 3.5
 REM set TCL_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tcl8.6
 REM set TK_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tk8.6
+@echo ............... FREEZING ...............
+C:\python34\python ..\cx_freeze\setup.py build -b cx_build > freezing.log
 
-C:\python34\python ..\cx_freeze\setup.py build > freezing.log
+@echo ............... DISABLING DPI AWARENESS ...............
+call DisableDPIAwareness %FREEZE_TARGET%\thonny_frontend.exe
+call DisableDPIAwareness %FREEZE_TARGET%\thonny_backend.exe
 
+@echo ............... BUILDING INSTALLER ...............
 set /p VERSION=<..\..\thonny\VERSION
-"C:\Program Files (x86)\Inno Setup 5\iscc" /dAppVer=%VERSION% inno_setup.iss > installer_building.log
+"C:\Program Files (x86)\Inno Setup 5\iscc" /dAppVer=%VERSION% /dSourceFolder=%FREEZE_TARGET% inno_setup.iss > installer_building.log
 pause
+
