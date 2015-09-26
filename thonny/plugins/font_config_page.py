fc74d241eedebde8c377e7ec1a0e3214d4f696ff
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-26 10:41:06 +0300

    Enhanced font configuration page

--- thonny/plugins/font_config_page.py
+++ thonny/plugins/font_config_page.py
@@ -6,6 +6,7 @@ from thonny.config_ui import ConfigurationPage
 from thonny.globals import get_workbench
 from tkinter.messagebox import showerror
 from thonny.ui_utils import create_string_var
+import textwrap
 
 
 class FontConfigurationPage(ConfigurationPage):
@@ -14,10 +15,12 @@ class FontConfigurationPage(ConfigurationPage):
         ConfigurationPage.__init__(self, master)
         
         self._family_variable = create_string_var(
-            get_workbench().get_option("view.editor_font_family"))
+            get_workbench().get_option("view.editor_font_family"),
+            modification_listener=self._update_preview_font)
         
         self._size_variable = create_string_var(
-            get_workbench().get_option("view.editor_font_size"))
+            get_workbench().get_option("view.editor_font_size"),
+            modification_listener=self._update_preview_font)
         
         ttk.Label(self, text="Editor font").grid(row=0, column=0, sticky="w")
         
@@ -29,35 +32,50 @@ class FontConfigurationPage(ConfigurationPage):
         self._family_combo.grid(row=1, column=0, sticky=tk.NSEW, padx=(0,10))
         
         ttk.Label(self, text="Size").grid(row=0, column=1, sticky="w")
-        self._size_entry = ttk.Entry(self, width=4, exportselection=False,
-                                     textvariable=self._size_variable)
+        self._size_combo = ttk.Combobox(self, width=4,
+                                        exportselection=False,
+                                        textvariable=self._size_variable,
+                                        state='readonly',
+                                        values=[str(x) for x in range(3,73)])
         
-        self._size_entry.grid(row=1, column=1)    
+        self._size_combo.grid(row=1, column=1)
+        
+        
+        ttk.Label(self, text="Preview").grid(row=2, column=0, sticky="w", pady=(10,0))
+        self._preview_font = tk_font.Font()
+        self._preview_text = tk.Text(self,
+                                height=10,
+                                borderwidth=1,
+                                font=self._preview_font,
+                                wrap=tk.WORD)
+        self._preview_text.insert("1.0", textwrap.dedent("""
+            The quick brown fox jumps over the lazy dog
+            
+            ABCDEFGHIJKLMNOPQRSTUVWXYZ
+            abcdefghijklmnopqrstuvwxyz
+            
+            1234567890
+            @$%()[]{}/\_-+
+            "Hello " + 'world!'""").strip())
+        self._preview_text.grid(row=3, column=0, columnspan=2, sticky=tk.NSEW, pady=(0,5))
+        
+            
         self.columnconfigure(0, weight=1)
+        self.rowconfigure(3, weight=1)
+        self._update_preview_font()
     
     def apply(self):
         if (not self._family_variable.modified
             and not self._size_variable.modified):
             return
         
-        min_font_size = 3
-        max_font_size = 99
-        
-        try:
-            size = int(self._size_variable.get())
-            assert min_font_size <= size <= max_font_size
-            
-            get_workbench().set_option("view.editor_font_size", size)
-            get_workbench().set_option("view.editor_font_family", self._family_variable.get())
-            get_workbench().update_fonts()
-            
-            return True
-        except:
-            showerror("Error", "Font size must be integer between %d and %d" 
-                      % (min_font_size, max_font_size))
-            self._size_entry.focus_set()
-            return False
+        get_workbench().set_option("view.editor_font_size", int(self._size_variable.get()))
+        get_workbench().set_option("view.editor_font_family", self._family_variable.get())
+        get_workbench().update_fonts()
     
+    def _update_preview_font(self):
+        self._preview_font.configure(family=self._family_variable.get(),
+                                     size=int(self._size_variable.get()))
     
     def _get_families_to_show(self):
         return sorted(filter(
@@ -66,4 +84,4 @@ class FontConfigurationPage(ConfigurationPage):
         ))
 
 def load_plugin():
-    get_workbench().add_configuration_page("Fonts", FontConfigurationPage)
+    get_workbench().add_configuration_page("Font", FontConfigurationPage)
