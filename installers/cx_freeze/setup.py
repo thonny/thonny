cae366827df26235515550db1562a83767c2a92e
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-09-30 11:51:50 +0300

    enhanced freezing script

--- installers/cx_freeze/setup.py
+++ installers/cx_freeze/setup.py
@@ -1,16 +1,35 @@
 import sys
 import os.path
 from cx_Freeze import setup, Executable
+import shutil
 
 
 MAIN_DIR = os.path.abspath("../..")
 
-print(os.getcwd())
-# Options shared by both Executables
+# make sure backend_private is up to date ------------------------
+bp_path = os.path.join(MAIN_DIR, "backend_private")
+os.makedirs(bp_path, 0o777, True)
+os.makedirs(os.path.join(bp_path, "thonny"), 0o777, True)
+
+for filename in ["thonny_backend.py",
+                 os.path.join("thonny", "backend.py"),
+                 os.path.join("thonny", "ast_utils.py"),
+                 os.path.join("thonny", "misc_utils.py"),
+                 os.path.join("thonny", "common.py")]:
+    original = os.path.join(MAIN_DIR, filename)
+    copy = os.path.join(bp_path, filename)
+    
+    if os.path.exists(original): 
+        shutil.copyfile(original, copy)
+        
+
+
+# Options shared by both Executables ----------------------------
 build_exe_options = {
     'path' : [MAIN_DIR] + sys.path,
-    'include_files': [MAIN_DIR + "/res",
-                      MAIN_DIR + "/VERSION"],
+    'include_files': [os.path.join(MAIN_DIR, "res"),
+                      os.path.join(MAIN_DIR , "VERSION"),
+                      os.path.join(MAIN_DIR, "backend_private")],
     'packages': ["jedi", "rope", "turtle", "idlelib", "thonny"],
     'include_msvcr' : True, 
     'base' : "Win32GUI" if sys.platform == "win32" else None,
