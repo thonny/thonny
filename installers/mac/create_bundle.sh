8a6687ea2249438afd718a307929ea6b82870f95
aivarannamaa <aivar.annamaa@gmail.com>
2015-10-02 02:08:14 +0300

    Fixed #76 with install_name_tool

--- installers/mac/create_bundle.sh
+++ installers/mac/create_bundle.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-# prepare working folder
+PLATFORM_ID=exe.macosx-10.6-intel-3.4
+
+# prepare working folder #########################################################
 rm -rf cx_build
 rm -rf build
 mkdir -p build
 
+# Freeze #########################################################################
 python3 ../cx_freeze/setup.py build -b cx_build > freezing.log
 
-# copy template and source files
+# fix absolute path dependency on Python #########################################
+install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.4/Python \
+	@executable_path/Python \
+	cx_build/$PLATFORM_ID/thonny_frontend
+	
+install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.4/Python \
+	@executable_path/Python \
+	cx_build/$PLATFORM_ID/thonny_backend
+
+# copy template and source files #################################################
 cp -r Thonny.app.template build/Thonny.app
 #mv build/Thonny.app.template build/Thonny.app
 
 mkdir -p build/Thonny.app/Contents/MacOS 
-cp -r cx_build/exe.macosx-10.6-intel-3.4/* build/Thonny.app/Contents/MacOS
+cp -r cx_build/$PLATFORM_ID/* build/Thonny.app/Contents/MacOS
 
-#chmod 755 build/Thonny.app/Contents/MacOS/thonny_launch
 
-# clean up unnecessary stuff
-#rm -rf build/Thonny.app/Contents/MacOs/thonny/__pycache__
-#rm -rf build/Thonny.app/Contents/MacOs/thonny/*.pyo
-#rm -rf build/Thonny.app/Contents/MacOs/thonny/*.pyc 
-
-# get and set version
+# get and set version ############################################################
 VERSION=$(<../../VERSION)
 sed -i.bak "s/VERSION/$VERSION/" build/Thonny.app/Contents/Info.plist
 rm -f build/Thonny.app/Contents/Info.plist.bak
 
 
-# create dmg
+# create dmg #####################################################################
 mkdir -p dist
 FILENAME=dist/thonny-$VERSION.dmg
 rm -f $FILENAME
