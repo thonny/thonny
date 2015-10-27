f52e64eb1db0e37c953c1109f143d21d946802ce
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-10-27 14:24:21 +0200

    reduced chattyness of windows builder

--- installers/windows/EnableDPIAwareness.bat
+++ installers/windows/EnableDPIAwareness.bat
@@ -1,8 +1,10 @@
+@echo off
+
 @SET PATH=%PATH%;C:\Program Files (x86)\Windows Kits\8.1\bin\x86;D:\PF\Windows Kits\10\bin\x86
 
-mt.exe -inputresource:%1;#1 -out:extracted.manifest
-mt.exe -manifest EnableDPIAwareness.manifest extracted.manifest -out:merged.manifest
-mt.exe -outputresource:%1;#1 -manifest merged.manifest
+mt.exe -inputresource:%1;#1 -out:extracted.manifest > nul
+mt.exe -manifest EnableDPIAwareness.manifest extracted.manifest -out:merged.manifest > nul
+mt.exe -outputresource:%1;#1 -manifest merged.manifest > nul
 
 del extracted.manifest
 del merged.manifest
--- installers/windows/create_installer.bat
+++ installers/windows/create_installer.bat
@@ -1,21 +1,25 @@
+@echo off
 set FREEZE_TARGET=cx_build\exe.win32-3.4
 
-@echo ............... CLEANING .................
+@echo ............... CLEANING ............................
 rmdir cx_build /s /q
 
 
 REM following two variables are fix for a bug in cx_freeze in Python 3.5
 REM set TCL_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tcl8.6
 REM set TK_LIBRARY=C:\Program Files (x86)\Python 3.5\tcl\tk8.6
-@echo ............... FREEZING ...............
+@echo ............... FREEZING ............................
 C:\python34\python ..\cx_freeze\setup.py build -b cx_build > freezing.log
 
-@echo ............... ENABLING DPI AWARENESS ...............
+@echo ............... ENABLING DPI AWARENESS ..............
 call EnableDPIAwareness %FREEZE_TARGET%\thonny_frontend.exe
 call EnableDPIAwareness %FREEZE_TARGET%\thonny_backend.exe
 
-@echo ............... BUILDING INSTALLER ...............
+@echo ............... BUILDING INSTALLER ..................
 set /p VERSION=<..\..\VERSION
 "C:\Program Files (x86)\Inno Setup 5\iscc" /dAppVer=%VERSION% /dSourceFolder=%FREEZE_TARGET% inno_setup.iss > installer_building.log
+
+
+@echo ............... DONE! ...............................
 pause
 
