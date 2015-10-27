705b37f30ecb2144838ef15c6f38c9c8e61cc0eb
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-10-27 14:34:08 +0200

    reminder for testing imports in frozen Thonny

---
+++ test/imports_to_test.py
@@ -0,0 +1,41 @@
+
+
+from random import randint    
+from hashlib import md5       
+from urllib import request    
+from pyexpat import model     
+
+import _bz2
+import _ctypes
+import _decimal
+import _elementtree
+import _hashlib
+import _multiprocessing
+import _overlapped
+import _socket
+import _sqlite3
+import _ssl
+import _tkinter
+
+import _testcapi
+import _testbuffer
+import _testimportmultiple
+import _ctypes_test
+
+import pygame.base
+import pygame.vidcap
+
+
+import pyexpat
+import select
+import unicodedata
+
+import os
+if os.name == "nt":
+    import win32api
+    import win32evtlog
+    import win32file
+    import win32pipe
+    import win32ui
+    import win32wnet
+    import winsound
