7592b9afc155170cc67c5379d210bdd54d231be0
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-11-10 18:44:14 +0200

    Added simple error reporting to thonny.exe

--- packaging/windows/ThonnyRunner/ThonnyRunner/thonny.cpp
+++ packaging/windows/ThonnyRunner/ThonnyRunner/thonny.cpp
@@ -16,12 +16,34 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 	Py_SetProgramName(argv[0]);
 	Py_Initialize();
 	PySys_SetArgvEx(argc, argv, 0);
-	int code = PyRun_SimpleString(
-		"from os.path import dirname, join\n"
-		"from runpy import run_path\n"
-		"import sys\n"
-		"run_path(join(dirname(sys.executable), 'thonny_frontend.py'))\n"
+
+	PyObject *py_main, *py_dict;
+	py_main = PyImport_AddModule("__main__");
+	py_dict = PyModule_GetDict(py_main);
+
+	PyObject* result = PyRun_String(
+		"from runpy import run_module\n"
+		"run_module('thonny')\n",
+		Py_file_input,
+		py_dict,
+		py_dict
 		);
+
+	int code;
+	if (!result) {
+		PyObject *ptype, *pvalue, *ptraceback;
+		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+
+		PyObject* valueAsString = PyObject_Str(pvalue);
+
+		wchar_t* error_msg = PyUnicode_AsWideCharString(valueAsString, NULL);
+		MessageBox(0, error_msg, L"Thonny startup error", MB_OK | MB_ICONERROR);
+		code = -1;
+	}
+	else {
+		code = 1;
+	}
+
 	Py_Finalize();
 
 	return code;
