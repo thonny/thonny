5cd88f554e2869323b843c8c8626fb66fd175312
aivarannamaa <aivar.annamaa@gmail.com>
2015-11-11 16:48:23 +0200

    Advances in Mac packaging

---
+++ packaging/mac/build_requirements.sh
@@ -0,0 +1,109 @@
+PREFIX=/opt/pythonny
+
+# COMPILE TCL/TK ####################################################################################
+# TODO
+
+
+
+
+# COMPILE LIBRARIES REQUIRED BY PYTHON ##############################################################
+# TODO
+
+
+# COMPILE PYTHON ####################################################################################
+# TODO
+
+
+# UPDATE LINKS ######################################################################################
+RELATIVE_LIBDIR=@executable_path/../lib
+
+# Tcl/Tk #############################
+cp -f /Library/Frameworks/Tcl.framework/Versions/8.6/Tcl $PREFIX/lib
+cp -f /Library/Frameworks/Tk.framework/Versions/8.6/Tk $PREFIX/lib
+chmod 655 $PREFIX/lib/Tcl
+chmod 655 $PREFIX/lib/Tk
+
+install_name_tool -id \
+	$RELATIVE_LIBDIR/Tcl \
+    $PREFIX/lib/Tcl 
+
+
+install_name_tool -id \
+	$RELATIVE_LIBDIR/Tk \
+    $PREFIX/lib/Tk 
+ 
+chmod 555 $PREFIX/lib/Tcl
+chmod 555 $PREFIX/lib/Tk
+
+mkdir $PREFIX/lib/tcl8.6
+mkdir $PREFIX/lib/tk8.6
+cp -R /Library/Frameworks/Tcl.framework/Versions/8.6/Resources/Scripts/ $PREFIX/lib/tcl8.6
+cp -R /Library/Frameworks/Tk.framework/Versions/8.6/Resources/Scripts/ $PREFIX/lib/tk8.6
+
+
+# _tkinter ##########################
+install_name_tool -change \
+    /Library/Frameworks/Tcl.framework/Versions/8.6/Tcl \
+	$RELATIVE_LIBDIR/Tcl \
+    $PREFIX/lib/python3.5/lib-dynload/_tkinter.cpython-35m-darwin.so 
+
+install_name_tool -change \
+    /Library/Frameworks/Tk.framework/Versions/8.6/Tk \
+	$RELATIVE_LIBDIR/Tk \
+    $PREFIX/lib/python3.5/lib-dynload/_tkinter.cpython-35m-darwin.so 
+
+# _ssl #################################
+cp -f /usr/local/opt/openssl/lib/libssl.1.0.0.dylib $PREFIX/lib
+cp -f /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib $PREFIX/lib
+
+chmod 655 $PREFIX/lib/libcrypto.1.0.0.dylib
+chmod 655 $PREFIX/lib/libssl.1.0.0.dylib
+
+install_name_tool -id \
+	$RELATIVE_LIBDIR/libcrypto.1.0.0.dylib \
+    $PREFIX/lib/libcrypto.1.0.0.dylib 
+
+install_name_tool -id \
+	$RELATIVE_LIBDIR/libssl.1.0.0.dylib \
+    $PREFIX/lib/libssl.1.0.0.dylib 
+
+
+install_name_tool -change \
+    /usr/local/opt/openssl/lib/libssl.1.0.0.dylib \
+	$RELATIVE_LIBDIR/libssl.1.0.0.dylib \
+    $PREFIX/lib/python3.5/lib-dynload/_ssl.cpython-35m-darwin.so 
+
+install_name_tool -change \
+    /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib \
+	$RELATIVE_LIBDIR/libcrypto.1.0.0.dylib \
+    $PREFIX/lib/python3.5/lib-dynload/_ssl.cpython-35m-darwin.so 
+
+# _lzma #################################
+install_name_tool -change \
+    /usr/local/lib/liblzma.5.dylib \
+	$RELATIVE_LIBDIR/liblzma.5.dylib \
+    $PREFIX/lib/python3.5/lib-dynload/_lzma.cpython-35m-darwin.so 
+
+
+
+# CLEAN UP ######################################################################################
+rm -rf $PREFIX/share
+rm -rf $PREFIX/include
+rm $PREFIX/bin/2to3-3.5
+rm $PREFIX/bin/easy_install-3.5
+rm $PREFIX/bin/idle3.5
+rm $PREFIX/bin/pip3.5
+rm $PREFIX/bin/pydoc3.5
+rm $PREFIX/bin/python3.5m
+rm $PREFIX/bin/python3.5m-config
+rm $PREFIX/bin/pyvenv-3.5
+
+rm $PREFIX/lib/libpython3.5m.a
+
+rm -rf $PREFIX/lib/python3.5/__pycache__
+rm -rf $PREFIX/lib/python3.5/config-3.5m # TODO: makefile is required
+rm -rf $PREFIX/lib/python3.5/test
+
+find $PREFIX/lib -name '*.pyc' -delete
+
+    
--- packaging/mac/create_bundle.sh
+++ packaging/mac/create_bundle.sh
@@ -1,56 +1,29 @@
 #!/bin/bash
 
-SOURCE_DIR=../..
-VERSION=$(<$SOURCE_DIR/VERSION)
+SOURCE_PREFIX=/opt/pythonny
+
+# update thonny installation #####################################################
+$SOURCE_PREFIX/bin/python3.5 -m pip install --force-reinstall thonny
+rm $SOURCE_PREFIX/bin/thonny
+
+
+VERSION=$(<$SOURCE_PREFIX/lib/python3.5/site-packages/thonny/VERSION)
 ARCHITECTURE="$(uname -m)"
 VERSION_NAME=thonny-$VERSION-$ARCHITECTURE 
 
 # prepare working folder #########################################################
-rm -rf cx_build
 rm -rf build
 mkdir -p build
 
 
-# Freeze #########################################################################
-PYTHON=/Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5
-$PYTHON ../cx_freeze/setup.py build -b cx_build > freezing.log
-
-# move some dirs to correct location ##################
-OUTPUT_DIR=cx_build/$(ls cx_build)
-mv $OUTPUT_DIR/lib/VERSION $OUTPUT_DIR
-mv $OUTPUT_DIR/lib/res $OUTPUT_DIR
-mv $OUTPUT_DIR/lib/backend_private $OUTPUT_DIR
-
-
-# fix absolute path dependency on Python #########################################
-install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.5/Python \
-	@executable_path/Python \
-	$OUTPUT_DIR/thonny_frontend
-	
-install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.5/Python \
-	@executable_path/Python \
-	$OUTPUT_DIR/thonny_backend
-
-install_name_tool -change /Library/Frameworks/Tcl.framework/Versions/8.6/Tcl \
-    @executable_path/../Frameworks/Tcl.framework/Versions/8.6/Tcl \
-    $OUTPUT_DIR/lib/python3.5/_tkinter.cpython-35m-darwin.so
-       
-install_name_tool -change /Library/Frameworks/Tk.framework/Versions/8.6/Tk \
-    @executable_path/../Frameworks/Tk.framework/Versions/8.6/Tk \
-    $OUTPUT_DIR/lib/python3.5/_tkinter.cpython-35m-darwin.so
-    
-#cp -R ~/Downloads/build/tcl/Tcl.framework/Versions/8.6/Resources/Scripts/* tcl
-       
 # copy template and source files #################################################
 cp -r Thonny.app.template build/Thonny.app
-#mv build/Thonny.app.template build/Thonny.app
 
 mkdir -p build/Thonny.app/Contents/MacOS 
-cp -r $OUTPUT_DIR/* build/Thonny.app/Contents/MacOS
+cp -r $SOURCE_PREFIX/* build/Thonny.app/Contents/MacOS
 
 
-# get and set version ############################################################
-VERSION=$(<../../VERSION)
+# set version ############################################################
 sed -i.bak "s/VERSION/$VERSION/" build/Thonny.app/Contents/Info.plist
 rm -f build/Thonny.app/Contents/Info.plist.bak
 
