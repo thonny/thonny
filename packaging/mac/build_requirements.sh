514e7a77c5057cdc588abe49c7ae00ab020e6a06
aivarannamaa <aivar.annamaa@gmail.com>
2015-11-18 17:29:58 +0200

    more mac build stuff

--- packaging/mac/build_requirements.sh
+++ packaging/mac/build_requirements.sh
@@ -5,20 +5,24 @@ export PREFIX=/Users/aivar/pythonny
 rm -Rf $PREFIX
 mkdir $PREFIX
 
-rm -Rf ./temp_build_dir
-mkdir ./temp_build_dir
-cd temp_build_dir
+
+MAIN_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+TEMP_BUILD_DIR=$MAIN_DIR/temp_build_dir
+rm -rf $TEMP_BUILD_DIR
+mkdir $TEMP_BUILD_DIR
+cd $TEMP_BUILD_DIR
 
 # NB! If you update xcode then first do:
 # sudo xcodebuild -license
 
 
-./build_tcltk.sh
-#./build_python.sh
-#./build_tkhtml.sh
-#./build_clean.sh
+$MAIN_DIR/build_tcltk.sh
+#$MAIN_DIR/build_python.sh
+#$MAIN_DIR/build_tkhtml.sh
+#$MAIN_DIR/build_clean.sh
 
 
-cd .. # back to original dir
+cd $MAIN_DIR # back to original dir
 
     
