107ac6ec9bbfcd7dde1b04c3171d7fe865b3f118
Aivar Annamaa <aivar.annamaa@gmail.com>
2015-11-24 15:27:56 +0200

    Moved cleaning Python to create_installer.bat

--- packaging/windows/build_requirements.bat
+++ packaging/windows/build_requirements.bat
@@ -7,45 +7,6 @@ mkdir %PREFIX%
 @echo ............... COPYING PYTHON ............................
 xcopy C:\Python35\* %PREFIX% /S /E /K>NUL
 
-@echo ............... CLEANING PYTHON ............................
-cd %PREFIX%
-move LICENSE.txt PYTHON-LICENSE.txt>NUL
-del README.txt>NUL
-del NEWS.txt>NUL
-del test.bat>NUL
-del /S *.pyc>NUL
-del /S *.lib>NUL
-del /S *.a>NUL
-del /S *.chm>NUL
 
-rmdir Doc /S /Q>NUL
-rmdir include /S /Q>NUL
-rmdir libs /S /Q>NUL
-rmdir Tools /S /Q>NUL
-rmdir Scripts /S /Q>NUL
-
-rmdir lib\test /S /Q>NUL
-rmdir lib\plat-* /S /Q>NUL
-
-
-del tcl\*.sh /Q>NUL
-del tcl\tcl8.6\clock.tcl>NUL
-del tcl\tcl8.6\safe.tcl>NUL
-rmdir tcl\tix8.4.3\demos /S /Q>NUL
-
-rmdir tcl\tk8.6\demos /S /Q>NUL
-rmdir tcl\tk8.6\msgs /S /Q>NUL
-
-rmdir tcl\tcl8.6\opt0.4 /S /Q>NUL
-rmdir tcl\tcl8.6\msgs /S /Q>NUL
-rmdir tcl\tcl8.6\tzdata /S /Q>NUL
-cd ..
-
-@echo ............... COPYING VS FILES ..........................
-xcopy ucrt_redist\*.dll %PREFIX% /S /E /K>NUL
-xcopy ucrt_redist\api-ms-win*.dll %PREFIX%\DLLs /S /E /K>NUL
-
-@echo ............... ENABLE DPI AWARNESS ..............................
-@REM call EnableDPIAwareness %PREFIX%\pythonw.exe
 
 pause
--- packaging/windows/create_installer.bat
+++ packaging/windows/create_installer.bat
@@ -1,18 +1,50 @@
 @echo off
 
-set PREFIX=pythonny
 set BUILDDIR=build
 rmdir %BUILDDIR% /S /Q
 mkdir %BUILDDIR%
 
 @echo ............... COPYING PYTHON ...................................
-xcopy %PREFIX%\* %BUILDDIR% /S /E /K>NUL
+xcopy pythonny\* %BUILDDIR% /S /E /K>NUL
 
 
 @echo ............... INSTALLING THONNY ...................................
 %BUILDDIR%\python.exe -m pip install --no-cache-dir --upgrade thonny
 
-@echo ............... CLEANING PYTHON ...................................
+
+@echo ............... CLEANING PYTHON ............................
+move %BUILDDIR%\LICENSE.txt %BUILDDIR%\PYTHON-LICENSE.txt>NUL
+del %BUILDDIR%\README.txt>NUL
+del %BUILDDIR%\NEWS.txt>NUL
+
+del /S "%BUILDDIR%\*.pyc">NUL
+del /S "%BUILDDIR%\*.lib">NUL
+del /S "%BUILDDIR%\*.a">NUL
+del /S "%BUILDDIR%\*.chm">NUL
+
+rmdir %BUILDDIR%\Doc /S /Q>NUL
+rmdir %BUILDDIR%\include /S /Q>NUL
+rmdir %BUILDDIR%\libs /S /Q>NUL
+rmdir %BUILDDIR%\Tools /S /Q>NUL
+rmdir %BUILDDIR%\Scripts /S /Q>NUL
+
+rmdir %BUILDDIR%\lib\test /S /Q>NUL
+rmdir %BUILDDIR%\lib\plat-* /S /Q>NUL
+
+
+del %BUILDDIR%\tcl\*.sh /Q>NUL
+del %BUILDDIR%\tcl\tcl8.6\clock.tcl>NUL
+del %BUILDDIR%\tcl\tcl8.6\safe.tcl>NUL
+rmdir %BUILDDIR%\tcl\tix8.4.3\demos /S /Q>NUL
+
+rmdir %BUILDDIR%\tcl\tk8.6\demos /S /Q>NUL
+rmdir %BUILDDIR%\tcl\tk8.6\msgs /S /Q>NUL
+
+rmdir %BUILDDIR%\tcl\tcl8.6\opt0.4 /S /Q>NUL
+rmdir %BUILDDIR%\tcl\tcl8.6\msgs /S /Q>NUL
+rmdir %BUILDDIR%\tcl\tcl8.6\tzdata /S /Q>NUL
+
+@echo ............... CLEANING PYTHON PIP ...................................
 rmdir %BUILDDIR%\Lib\ensurepip /S /Q>NUL
 rmdir %BUILDDIR%\Lib\site-packages\pip /S /Q>NUL
 rmdir %BUILDDIR%\Lib\site-packages\pip-7.1.2.dist-info /S /Q>NUL
@@ -25,6 +57,14 @@ rmdir %BUILDDIR%\Lib\site-packages\requests_toolbelt /S /Q>NUL
 rmdir %BUILDDIR%\Lib\site-packages\requests_toolbelt-0.4.0.dist-info /S /Q>NUL
 rmdir %BUILDDIR%\Lib\site-packages\requests-2.8.1.dist-info /S /Q>NUL
 
+
+
+@echo ............... COPYING VS FILES ..........................
+xcopy ucrt_redist\*.dll %BUILDDIR% /S /E /K>NUL
+xcopy ucrt_redist\api-ms-win*.dll %BUILDDIR%\DLLs /S /E /K>NUL
+
+@echo ............... ENABLE DPI AWARNESS ..............................
+@REM call EnableDPIAwareness %PREFIX%\pythonw.exe
 
 @echo ............... COPYING THONNY LAUNCHER ..........................
 copy ThonnyRunner\Release\thonny.exe %BUILDDIR% /Y
