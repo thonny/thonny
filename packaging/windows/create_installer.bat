dc8a0c27a42747a35a158f9b0f44634ab010655b
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-01-12 18:19:18 +0200

    Win installer version was taken from wrong place

--- packaging/windows/create_installer.bat
+++ packaging/windows/create_installer.bat
@@ -72,7 +72,7 @@ copy ..\..\*LICENSE.txt %BUILDDIR% /Y>NUL
 
 
 @echo ............... CREATING INSTALLER ..........................
-set /p VERSION=<..\..\thonny\VERSION
+set /p VERSION=<%BUILDDIR%\Lib\site-packages\thonny\VERSION
 "C:\Program Files (x86)\Inno Setup 5\iscc" /dAppVer=%VERSION% /dSourceFolder=build inno_setup.iss > installer_building.log
 
 
