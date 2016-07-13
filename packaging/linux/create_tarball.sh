3e369f27268ea28677d6a1b28fc66cb1a010ce2e
aivar <aivar.annamaa@gmail.com>
2016-07-13 10:26:39 +0300

    changed deletions in Linux create_tarball

--- packaging/linux/create_tarball.sh
+++ packaging/linux/create_tarball.sh
@@ -41,21 +41,34 @@ cp thonny $TARGET_DIR/bin
 
 
 # clean up unnecessary stuff
+rm -rf $TARGET_DIR/share
+rm -rf $TARGET_DIR/man
+rm -rf $TARGET_DIR/openssl/man
+rm -rf $TARGET_DIR/include/openssl
+
+rm -rf $TARGET_DIR/bin/python3.5m # too big
+rm -rf $TARGET_DIR/bin/openssl
+rm -rf $TARGET_DIR/bin/*z*
+rm -rf $TARGET_DIR/bin/c_rehash
+rm -rf $TARGET_DIR/bin/2to3*
+rm -rf $TARGET_DIR/bin/idle*
+rm -rf $TARGET_DIR/bin/pip* # TODO: don't delete but replace shebang
+rm -rf $TARGET_DIR/bin/pydoc*
+rm -rf $TARGET_DIR/bin/pyvenv*
+rm -rf $TARGET_DIR/bin/tclsh*
+rm -rf $TARGET_DIR/bin/wish*
+
+
 #find $TARGET_DIR -type f -name "*.a" -delete
 find $TARGET_DIR -type f -name "*.pyo" -delete
 find $TARGET_DIR -type f -name "*.pyc" -delete
 find $TARGET_DIR -type d -name "__pycache__" -delete
 
+rm -rf $TARGET_DIR/lib/python3.5/ensurepip
 #rm -rf $TARGET_DIR/include
 #rm -rf $TARGET_DIR/lib/python3.5/config-3.5m
-rm -rf $TARGET_DIR/lib/python3.5/ensurepip
 #rm -rf $TARGET_DIR/lib/python3.5/site-packages/pip*
 #rm -rf $TARGET_DIR/lib/python3.5/site-packages/setuptools*
-rm -rf $TARGET_DIR/openssl/man
-
-rm -rf $TARGET_DIR/bin/openssl
-rm -rf $TARGET_DIR/bin/*z*
-rm -rf $TARGET_DIR/bin/c_rehash
 
 rm -rf $TARGET_DIR/lib/python3.5/site-packages/tkinterhtml/tkhtml/Windows
 rm -rf $TARGET_DIR/lib/python3.5/site-packages/tkinterhtml/tkhtml/MacOSX
