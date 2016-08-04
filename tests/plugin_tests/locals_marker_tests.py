0cf64317228f76aa95f4c1ce5ed54cfa50931476
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-08-04 11:39:27 +0300

    updated tests according to prev commit

--- tests/plugin_tests/locals_marker_tests.py
+++ tests/plugin_tests/locals_marker_tests.py
@@ -1,6 +1,6 @@
 import tkinter
 
-from thonny.plugins.global_local_marker import GlobLocHighlighter
+from thonny.plugins.locals_marker import LocalsHighlighter
 
 TEST_STR1 = """num_cars = 3
 def foo():
@@ -16,29 +16,18 @@ def joo():
 
 def test_regular_closed():
 
-    expected_global = {('1.0', '1.8'),
-                       ('2.4', '2.7'),
-                       ('3.4', '3.9'),
-                       ('3.10', '3.18'),
-                       ('3.21', '3.29'),
-                       ('6.4', '6.9'),
-                       ('4.4', '4.7'),
-                       ('7.4', '7.7'), }
     expected_local = {('5.4', '5.12'),
                       ('6.10', '6.18'),
                       ('6.21', '6.29'),
-                      ('8.11', '8.19'),
-                      ('9.4', '9.12'),
                       }
 
     text_widget = tkinter.Text()
     text_widget.insert("end", TEST_STR1)
 
-    highlighter = GlobLocHighlighter(text_widget)
+    highlighter = LocalsHighlighter(text_widget)
 
-    actual_global, actual_local = highlighter.get_positions()
+    actual_local = highlighter.get_positions()
 
-    assert actual_global == expected_global
     assert actual_local == expected_local
     print("Passed.")
 
