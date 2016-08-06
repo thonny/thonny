0ec69ef2c58ecd85c16abcfd391180cad5a450b4
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-08-06 08:39:24 +0300

    autocompletions are now computed on the backend

--- thonny/plugins/autocomplete.py
+++ thonny/plugins/autocomplete.py
@@ -1,15 +1,16 @@
-import __main__  # @UnresolvedImport
 import tkinter as tk
-import jedi
-from jedi import Interpreter
-from thonny.globals import get_workbench
+from thonny.globals import get_workbench, get_runner
 from thonny.codeview import CodeViewText
 from thonny.shell import ShellText
+from thonny.common import InlineCommand
+from textwrap import dedent
 
 
 
 # TODO: adjust the window position in cases where it's too close to bottom or right edge - but make sure the current line is shown
-
+"""Completions get computed on the backend, therefore getting the completions is
+asynchronous.
+"""
 class Completer(tk.Listbox):
     def __init__(self, text):
         tk.Listbox.__init__(self, master=text,
@@ -17,8 +18,8 @@ class Completer(tk.Listbox):
                             activestyle="dotbox",
                             exportselection=False)
         
-        self.completions = []
         self.text = text
+        self.completions = []
         
         # Auto indenter will eat up returns, therefore I need to raise the priority
         # of this binding
@@ -27,59 +28,101 @@ class Completer(tk.Listbox):
         self.text.bind_class(self.text_priority_bindtag, "<Key>", self._on_text_keypress, True)
         
         self.text.bind("<<TextChange>>", self._on_text_change, True) # Assuming TweakableText
+        
+        # TODO: remove binding when editor gets closed
+        get_workbench().bind("InlineResult", self._handle_inline_result, True)
     
     def handle_autocomplete_request(self):
-        if self._is_visible():
+        row, column = self._get_position()
+        source = self.text.get("1.0", "end-1c")
+        
+        backend_code = dedent("""\
+        try:
+            import jedi
+            script = jedi.Script(source, row, column, filename)
+            completions = [{"name":c.name, "complete":c.complete}
+                            for c in script.completions()]
+        except ImportError:
+            completions = [{"name":"", "complete":"<could not import jedi>"}]
+        
+        __result__ = {
+            "source"   : source,
+            "row"      : row,
+            "column"   : column,
+            "filename" : filename,
+            "completions" : completions
+        }
+        """)
+        
+        get_runner().send_command(InlineCommand(command="execute_source",
+                                                source=backend_code,
+                                                request_id=self._get_request_id(),
+                                                global_vars={"source" : source,
+                                                             "row" : row,
+                                                             "column" : column,
+                                                             "filename" : self._get_filename()}))
+    
+    def _handle_inline_result(self, msg):
+        if msg.request_id != self._get_request_id():
             return
         
         row, column = self._get_position()
-        self._update_completions()
-        
-        if len(self.completions) == 0:
-            pass
-        elif len(self.completions) == 1:
-            self._insert_completion(self.completions[0]) #insert the only completion
+        result = msg.__result__
+        print("RESULT", result)
+        # check if the response is relevant for current state
+        if (result["source"] == self.text.get("1.0", "end-1c")
+            and result["row"] == row and result["column"] == column):
+            self._present_completions(result["completions"])
         else:
-            self._show(self.completions)        
+            self._close()
+            
+    def _present_completions(self, completions):
+        self.completions = completions
         
-        
-        get_workbench().event_generate("AutocompleteQuery",
+        # broadcast logging info
+        row, column = self._get_position()
+        get_workbench().event_generate("AutocompleteProposal",
             text_widget=self.text,
             row=row,
             column=column,
-            proposed_names=[c.name for c in self.completions])
-    
-    def _handle_inline_result(self, event):
-        "TODO: extract data, update completions and so on"
+            proposal_count=len(completions))
         
-    def _show(self, completions):
-        typed_name_length = len(completions[0].name) - len(completions[0].complete)
-        box_x, box_y, _, box_height = self.text.bbox('insert-%dc' % typed_name_length);
-        self.place(x=box_x, y=box_y + box_height,
-                   width=400, height=200)
+        # present
+        if len(completions) == 0:
+            self._close()
+        elif len(completions) == 1:
+            self._insert_completion(completions[0]) #insert the only completion
+            self._close()
+        else:
+            self._show_box(completions)
+        
+            
+    def _show_box(self, completions):
+        self.delete(0, self.size())
+        self.insert(0, *[c["name"] for c in completions])
+        self.activate(0)
+        self.selection_set(0)
+        
+        # place box
+        if not self._is_visible():
+            typed_name_length = len(completions[0]["name"]) - len(completions[0]["complete"])
+            box_x, box_y, _, box_height = self.text.bbox('insert-%dc' % typed_name_length);
+            self.place(x=box_x, y=box_y + box_height,
+                       width=400, height=200)
 
     def _is_visible(self):
         return self.winfo_ismapped()
     
-    def _insert_completion(self, completion=None):
-        if completion is None:
-            completion = self.completions[self.curselection()[0]]
-        
+    def _insert_completion(self, completion):
         get_workbench().event_generate("AutocompleteInsertion",
             text_widget=self.text,
-            typed_name=completion.name[:-len(completion.complete)],
-            completed_name=completion.name)
+            typed_name=completion["name"][:-len(completion["complete"])],
+            completed_name=completion["name"])
         
         if self._is_visible():
             self._close()
             
-        self.text.insert(self.text.index('insert'), completion.complete)
-        
-        try:
-            print(completion.docstring())
-        except:
-            # jedi crashes in some cases
-            pass
+        self.text.insert(self.text.index('insert'), completion["complete"])
         
     
     def _get_filename(self):
@@ -109,21 +152,9 @@ class Completer(tk.Listbox):
         self.selection_set(index)
         self.activate(index)
     
-    def _update_completions(self):
-        self.completions = self._get_completions()
-        names = [c.name for c in self.completions]
-        self.delete(0, self.size())
-        self.insert(0, *names)
-        self.activate(0)
-        self.selection_set(0)
+    def _get_request_id(self):
+        return "autocomplete_" + str(self.text.winfo_id())
     
-    def _get_completions(self):
-        row, column = self._get_position()
-        source = self.text.get("1.0", "end-1c")
-        script = jedi.Script(source, row, column, self._get_filename())
-        return script.completions() 
-        
-
     def _get_position(self):
         return map(int, self.text.index("insert").split("."))
     
@@ -141,23 +172,56 @@ class Completer(tk.Listbox):
             self._move_selection(1)
             return "break"
         elif event.keysym in ["Return", "KP_Enter"]:
-            self._insert_completion()
+            assert self.size() > 0
+            self._insert_completion(self.completions[(self.curselection()[0])])
             return "break"
     
     def _on_text_change(self, event=None):
         if self._is_visible():
-            self._update_completions()
+            self.handle_autocomplete_request()
 
     
     def _close(self, event=False):
         self.place_forget()
 
 class ShellCompleter(Completer):
-    def _get_completions(self):
-        source = self.text.get("insert linestart", "insert") # TODO: allow multiple line input
-        interpreter = Interpreter(source, [__main__.__dict__])
-        return interpreter.completions()
+    def handle_autocomplete_request(self):
+        backend_code = dedent("""\
+        import __main__
+        try:
+            import jedi
+            interpreter = jedi.Interpreter(source, [__main__.__dict__])
+            completions = [{"name":c.name, "complete":c.complete}
+                            for c in interpreter.completions()]
+        except ImportError:
+            completions = [{"name":"", "complete":"<could not import jedi>"}]
+        
+        __result__ = {
+            "source"   : source,
+            "completions" : completions
+        }
+        """)
+        
+        get_runner().send_command(InlineCommand(command="execute_source",
+                                                source=backend_code,
+                                                request_id=self._get_request_id(),
+                                                extra_vars={"source" : self._get_prefix()}))
+    
+    def _handle_inline_result(self, msg):
+        if msg.request_id != self._get_request_id():
+            return
+        
+        # check if the response is relevant for current state
+        if msg.__result__["source"] == self._get_prefix():
+            self._present_completions(msg.__result__["completions"])
+        else:
+            self._close()
 
+
+    def _get_prefix(self):
+        return self.text.get("insert linestart", "insert") # TODO: allow multiple line input
+
+        
 def handle_autocomplete_request(event=None):
     if event is None:
         text = get_workbench().focus_get()
