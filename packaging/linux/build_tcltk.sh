1feabd881619a1d58c1bf20bca17706e909015ac
Aivar <aivar@aivar-virtual-machine.(none)>
2016-08-08 23:15:24 +0300

    tcl 8.6.6 on linux

--- packaging/linux/build_tcltk.sh
+++ packaging/linux/build_tcltk.sh
@@ -4,17 +4,18 @@ set -e
 
 # COMPILE TCL/TK ####################################################
 # wget http://prdownloads.sourceforge.net/tcl/tcl8.6.5-src.tar.gz
-wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl8.6.5-src.tar.gz
-tar -xzf tcl8.6.5-src.tar.gz
-cd tcl8.6.5/unix
+VERSION=8.6.6
+wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl$VERSION-src.tar.gz
+tar -xzf tcl$VERSION-src.tar.gz
+cd tcl$VERSION/unix
 ./configure --prefix=$PREFIX
 make install
 cd ../..
 
 # wget http://prdownloads.sourceforge.net/tcl/tk8.6.5-src.tar.gz
-wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk8.6.5-src.tar.gz
-tar -xzf tk8.6.5-src.tar.gz
-cd tk8.6.5/unix
+wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk$VERSION-src.tar.gz
+tar -xzf tk$VERSION-src.tar.gz
+cd tk$VERSION/unix
 # see http://sourceforge.net/p/tktoolkit/bugs/2588/ for --disable-xss
 ./configure --prefix=$PREFIX --disable-xss 
 make install
