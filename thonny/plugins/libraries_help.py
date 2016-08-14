bea6f93c307e3f0c1c4938572668a4e14a2962a4
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-08-14 10:15:01 +0300

    updated libraries help link

--- thonny/plugins/libraries_help.py
+++ thonny/plugins/libraries_help.py
@@ -1,9 +1,6 @@
 from thonny.globals import get_workbench
 import webbrowser
 def load_plugin():
-    get_workbench().add_command("pygame_help", "help", "Using Pygame",
-                                lambda: webbrowser.open("https://bitbucket.org/plas/thonny/wiki/Pygame"),
-                                group=30)
-    get_workbench().add_command("pygame_help", "help", "Using scientific libraries",
-                                lambda: webbrowser.open("https://bitbucket.org/plas/thonny/wiki/ScientificPython"),
+    get_workbench().add_command("pygame_help", "help", "Installing extra packages",
+                                lambda: webbrowser.open("https://bitbucket.org/plas/thonny/wiki/InstallingPackages"),
                                 group=30)
