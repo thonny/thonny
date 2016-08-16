e6113723acf0fc77304c7090bbcb0c6b9804f04f
Aivar Annamaa <aivar.annamaa@gmail.com>
2016-08-16 10:34:19 +0300

    small experiment

---
+++ misc/iterf.py
@@ -0,0 +1,26 @@
+import ast
+prog = """
+try:
+    3
+except:
+    "adf"
+"""
+
+root = ast.parse(prog)
+
+def print_ast(node, level):
+    print(" " * level, node)
+    for name, child in ast.iter_fields(node):
+        if isinstance(child, ast.AST):
+            print(" " * level, name, ":")
+            print_ast(child, level+1)
+        elif isinstance(child, list):
+            print(" " * level, name, ":[")
+            for elem in child:
+                print_ast(elem, level+1)
+            print(" " * level, "]")
+        else:
+            pass
+            #print(" " * level, "OOOO", node)
+
+print_ast(root, 0)
