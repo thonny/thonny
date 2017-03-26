a9619eed6ac2b0ae48088fe2642d37c276898564
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-03-26 16:45:39 +0300

    experiments

--- thonny/plugins/pip_gui.py
+++ thonny/plugins/pip_gui.py
@@ -14,15 +14,19 @@ from thonny.globals import get_workbench
 import subprocess
 import collections
 import threading
+from urllib.request import urlopen
+from concurrent.futures.process import ProcessPoolExecutor
+from concurrent.futures.thread import ThreadPoolExecutor
 
 class PipDialog(tk.Toplevel):
     def __init__(self, master):
         tk.Toplevel.__init__(self, master)
         
-        #self.geometry("200x200")
-        # TODO: position in the center of master
-        self.geometry("600x400+%d+%d" % (master.winfo_rootx() + master.winfo_width() // 2 - 50,
-                                  master.winfo_rooty() + master.winfo_height() // 2 - 150))
+        width = 600
+        height = 400
+        self.geometry("%dx%d+%d+%d" % (width, height,
+            master.winfo_rootx() + master.winfo_width() // 2 - width//2,
+            master.winfo_rooty() + master.winfo_height() // 2 - height//2))
 
         main_frame = tk.Frame(self)
         main_frame.grid(sticky=tk.NSEW, ipadx=15, ipady=15)
@@ -34,12 +38,13 @@ class PipDialog(tk.Toplevel):
             self.configure(background="systemSheetBackground")
         #self.resizable(height=tk.FALSE, width=tk.FALSE)
         self.transient(master)
-        #self.grab_set()
+        self.grab_set() # to make it active
+        self.grab_release() # to allow eg. copy something from the editor 
         self.protocol("WM_DELETE_WINDOW", self._ok)
         
         self._create_widgets(main_frame)
         
-        #ok_button.focus_set()
+        self.search_box.focus_set()
         
         self.bind('<Return>', self._ok, True) 
         self.bind('<Escape>', self._ok, True) 
@@ -53,8 +58,8 @@ class PipDialog(tk.Toplevel):
         
         name_font = tk.font.nametofont("TkDefaultFont").copy()
         #name_font.configure(size=16)
-        self.name_entry = ttk.Entry(header_frame, font=name_font)
-        self.name_entry.grid(row=0, column=0, sticky="nsew")
+        self.search_box = ttk.Entry(header_frame, font=name_font)
+        self.search_box.grid(row=0, column=0, sticky="nsew")
         
         self.search_button = ttk.Button(header_frame, text="Search")
         self.search_button.grid(row=0, column=1, sticky="nse", padx=(10,0))
@@ -101,22 +106,48 @@ Requires:""")
         command_frame = ttk.Frame(infoframe)
         command_frame.grid(row=2, column=0, sticky="w")
         
-        install_button = ttk.Button(command_frame, text="Install")
+        install_button = ttk.Button(command_frame, text="  Upgrade  (2.0.7 → 2.1.0)  ")
         install_button.grid(row=0, column=0, sticky="w")
         
         upgrade_button = ttk.Button(command_frame, text="Upgrade")
-        upgrade_button.grid(row=0, column=1, sticky="w")
+        #upgrade_button.grid(row=0, column=1, sticky="w")
         
         uninstall_button = ttk.Button(command_frame, text="Uninstall")
-        uninstall_button.grid(row=0, column=2, sticky="w")
+        #uninstall_button.grid(row=0, column=2, sticky="w")
     
         close_button = ttk.Button(infoframe, text="Close")
         close_button.grid(row=2, column=3, sticky="e")
         
+        self._load_package_info("thonny", "2.0.7")
+    
+    
+    def _load_package_info(self, name, version):
+        url = "https://pypi.python.org/pypi/{}/{}/json".format(name, version)
+        fut = _fetch_url_future(url)
+        self._check_update_info(fut)
+        
+    
+    def _check_update_info(self, fut):
+        if fut.done():
+            data = fut.result().decode("UTF-8")
+            self.info_text.insert("end", "\n" + data)
+        else:
+            self.after(200, self._check_update_info, fut)
+            
+        
+    
     def _ok(self, event=None):
         self.destroy()
 
 
+def _fetch_url_future(url, timeout=10):
+    def load_url():
+        with urlopen(url, timeout=timeout) as conn:
+            return conn.read()
+            
+    executor = ThreadPoolExecutor(max_workers=1)
+    return executor.submit(load_url)
+    
 
 def _direct_system_command_to_queue(cmd, event_queue):
     encoding = "UTF-8"
