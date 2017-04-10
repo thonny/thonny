1ba22cc562e4421e79777153b912369b0644e9f4
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-04-10 17:49:05 +0300

    Fixed #157 (Bring out Python version requirements in Options => Interpreter)

--- thonny/plugins/interpreter_config_page.py
+++ thonny/plugins/interpreter_config_page.py
@@ -33,7 +33,8 @@ class InterpreterConfigurationPage(ConfigurationPage):
         another_label.grid(row=2, column=0, columnspan=2, sticky=tk.W, pady=(10,0))
         self._select_button = ttk.Button(self,
                                          text="Locate another executable "
-                                         + ("(pythonw.exe) ..." if running_on_windows() else "(python3) ..."),
+                                         + ("(pythonw.exe) ..." if running_on_windows() else "(python3) ...")
+                                         + "\nNB! Thonny only supports Python 3.4 and later",
                                          command=self._select_executable)
         
         self._select_button.grid(row=3, column=0, columnspan=2, sticky=tk.NSEW)
