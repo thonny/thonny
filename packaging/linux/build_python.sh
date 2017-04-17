0ad89af062ce598a95b77b4a13d6aef56696c083
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-04-17 18:14:21 +0300

    Build Python 3.6.1 on Linux

--- packaging/linux/build_python.sh
+++ packaging/linux/build_python.sh
@@ -4,7 +4,7 @@ set -e
 
 # COMPILE PYTHON ###############################################
 
-PYTHON_VERSION=3.6.0
+PYTHON_VERSION=3.6.1
 RELEASE_NAME=Python-${PYTHON_VERSION}
 
 wget https://www.python.org/ftp/python/$PYTHON_VERSION/$RELEASE_NAME.tar.xz
