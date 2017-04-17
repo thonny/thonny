2c9917f42b02310effa7d36976a78ab16271d5ea
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-04-17 09:42:31 +0300

    more jedi comp

--- thonny/jedi_utils.py
+++ thonny/jedi_utils.py
@@ -1,8 +1,7 @@
 def get_module_node(script):
-    if hasattr(script, "_get_modsule_node"):
+    if hasattr(script, "_get_module_node"):
         return script._get_module_node()
     elif hasattr(script, "_get_module"):
-        m = script._get_module()
-        return m.tree_node
+        return script._get_module()
     else:
         return script._parser.module()
