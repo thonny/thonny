709cafa4192ac3a55d56280a5ca6828b2a9c0e51
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-05-04 22:41:05 +0300

    clean before creating dist

--- packaging/setuptools/create_dist.bat
+++ packaging/setuptools/create_dist.bat
@@ -1,6 +1,8 @@
 @echo ............... CHANGING TO PROJECT ROOT ......................
 cd ..\..
 
+rmdir build /s /q
+
 @echo ............... CREATING wheel ................................
 C:\Python36-32\python.exe setup.py bdist_wheel -d packaging\setuptools
 
