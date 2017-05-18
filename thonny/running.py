9f145d67ecc859f519d0c7af128adb4a77c2b887
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-05-18 21:14:16 +0300

    Fixed #267 (Kivy process doesn't like subprocess.STARTF_USESHOWWINDOW)
    subprocess.STARTF_USESHOWWINDOW is not required anymore

--- thonny/running.py
+++ thonny/running.py
@@ -679,11 +679,8 @@ class CPythonProxy(BackendProxy):
             my_env.update(cmd.environment)            
         
         creationflags = 0
-        startupinfo = None
         if running_on_windows():
             creationflags = subprocess.CREATE_NEW_PROCESS_GROUP
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         
         debug("Starting the backend: %s %s", cmd_line, self.cwd)
         self._proc = subprocess.Popen (
@@ -695,8 +692,7 @@ class CPythonProxy(BackendProxy):
             cwd=self.cwd,
             env=my_env,
             universal_newlines=True,
-            creationflags=creationflags,
-            startupinfo=startupinfo
+            creationflags=creationflags
         )
         
         if cmd:
