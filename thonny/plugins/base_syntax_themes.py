e1f1e9a62a5a2bb9023f4c10fbfa24e0a5bf7a75
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-03-24 00:12:43 +0200

    base syntax theme

--- thonny/plugins/base_syntax_themes.py
+++ thonny/plugins/base_syntax_themes.py
@@ -9,16 +9,16 @@ def default_light():
         "definition"    : {"foreground" : default_fg},
         "magic"         : {"foreground" : default_fg},
         "string"        : {"foreground" : string_fg},
-        #"string3"       : {"foreground" : string_fg},
+        "string3"       : {"foreground" : string_fg},
         "open_string"   : {"foreground" : string_fg, "background" : open_string_bg},
-        #"open_string3"  : {"foreground" : string_fg, "background" : open_string_bg},
+        "open_string3"  : {"foreground" : string_fg, "background" : open_string_bg},
         "keyword"       : {"foreground" : "#7f0055", "font" : "BoldEditorFont"},
-        "number"        : {"foreground" : default_fg},
+        "number"        : {"foreground" : "red"},
         "comment"       : {"foreground" : default_fg}
     }
 
 def default_dark():
-    default_fg = "DarkGray"
+    default_fg = "white"
     string_fg = "DarkGreen"
     open_string_bg = "#c3f9d3"
     
@@ -26,16 +26,16 @@ def default_dark():
         "definition"    : {"foreground" : default_fg},
         "magic"         : {"foreground" : default_fg},
         "string"        : {"foreground" : string_fg},
-        #"string3"       : {"foreground" : string_fg},
+        "string3"       : {"foreground" : string_fg},
         "open_string"   : {"foreground" : string_fg, "background" : open_string_bg},
-        #"open_string3"  : {"foreground" : string_fg, "background" : open_string_bg},
-        "keyword"       : {"foreground" : "#7f0055", "font" : "BoldEditorFont"},
+        "open_string3"  : {"foreground" : string_fg, "background" : open_string_bg},
+        "keyword"       : {"foreground" : "pink", "font" : "BoldEditorFont"},
         "number"        : {"foreground" : default_fg},
         "comment"       : {"foreground" : default_fg}
     }
 
 def load_early_plugin():
     get_workbench().add_syntax_theme("Default light", None, default_light)
-    get_workbench().add_syntax_theme("Default dark", None, default_light)
+    get_workbench().add_syntax_theme("Default dark", None, default_dark)
     get_workbench().set_default("view.syntax_theme", "Default light")
     
