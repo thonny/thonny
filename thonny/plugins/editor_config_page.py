0f66ce38bc98b9f31826c6998c3c3f148c50021b
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-06-14 20:58:42 +0300

    Make line numbers option visible again

--- thonny/plugins/editor_config_page.py
+++ thonny/plugins/editor_config_page.py
@@ -35,13 +35,13 @@ class EditorConfigurationPage(ConfigurationPage):
         self.add_checkbox("view.show_line_numbers", "Show line numbers", pady=(20,0))
         self._line_length_var = get_workbench().get_variable("view.recommended_line_length")
         label = ttk.Label(self, text="Recommended maximum line length\n(Set to 0 to turn off margin line)")
-        label.grid(row=7, column=0, sticky=tk.W)
+        label.grid(row=20, column=0, sticky=tk.W)
         self._line_length_combo = ttk.Combobox(self, width=4,
                                         exportselection=False,
                                         textvariable=self._line_length_var,
                                         state='readonly',
                                         values=[0,60,70,80,90,100,110,120])
-        self._line_length_combo.grid(row=7, column=1, sticky=tk.E)
+        self._line_length_combo.grid(row=20, column=1, sticky=tk.E)
         
         self.columnconfigure(0, weight=1)
     
