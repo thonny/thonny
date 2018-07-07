30a87f8e25fbaa8bff2b7f3a0df7a5759dd055b9
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-07-07 09:09:22 +0300

    Dock user windows

---
+++ thonny/plugins/backend/dock_user_windows_backend.py
@@ -0,0 +1,58 @@
+import os
+from thonny.backend import get_vm
+from thonny.common import BackendEvent
+
+_last_pos = (None, None)
+_notification_is_sent = False
+_LAST_POS_SETTING = "dock_user_windows.last_position"
+
+def on_configure(event):
+    global _last_pos, _notification_is_sent
+    pos = event.x, event.y
+    if pos != _last_pos:
+        get_vm().set_option(_LAST_POS_SETTING, pos)
+    
+    if not _notification_is_sent:
+        get_vm().send_message(BackendEvent("UserWindowAppeared"))
+        _notification_is_sent = True
+        
+
+def patch_tkinter(module):
+    flag_name = "has_docking_patch"
+    if getattr(module, flag_name, False):
+        return
+    
+    original_constructor = module.Tk.__init__
+    
+    def patched_Tk_constructor(self, *args, **kw):
+        original_constructor(self, *args, **kw)
+        
+        # move window to the same place it was previously
+        last_pos = get_vm().get_option(_LAST_POS_SETTING)
+        if isinstance(last_pos, tuple):
+            self.geometry("+%d+%d" % last_pos)
+            
+        self.wm_attributes("-topmost", 1)
+        #self.overrideredirect(1)
+        
+        # I'm using bind_class because turtle._Screen later overwrites the bind handler
+        self.bind_class("Tk", "<Configure>", on_configure, True)
+        
+    
+    module.Tk.__init__ = patched_Tk_constructor
+    setattr(module, flag_name, True)
+
+def patch_turtle(module):
+    # Turtle needs more tweaking because it later overrides the position set in the Tk constructor
+    turtle_config = getattr(module, "_CFG", None) 
+    if isinstance(turtle_config, dict):
+        last_pos = get_vm().get_option(_LAST_POS_SETTING)
+        if isinstance(last_pos, tuple):
+            turtle_config["leftright"], turtle_config["topbottom"] = last_pos 
+
+def load_plugin():
+    if os.environ.get("DOCK_USER_WINDOWS", "False").lower() == "true":
+        vm = get_vm()
+        vm.add_import_handler("tkinter", patch_tkinter)
+        vm.add_import_handler("turtle", patch_turtle)
+    
---
+++ thonny/plugins/dock_user_windows_frontend.py
@@ -0,0 +1,26 @@
+import os
+from thonny import get_workbench
+
+_OPTION_NAME = "run.dock_user_windows"
+
+def toggle_variable():
+    var = get_workbench().get_variable(_OPTION_NAME)
+    var.set(not var.get())
+    update_environment()
+
+def update_environment():
+    os.environ["DOCK_USER_WINDOWS"] = str(get_workbench().get_option(_OPTION_NAME))
+
+def on_window_appear(event):
+    get_workbench().become_active_window(force=False)
+
+def load_plugin():
+    get_workbench().set_default(_OPTION_NAME, False)
+    get_workbench().add_command("toggle_dock_user_windows", "run", 
+                                "Dock user windows",
+                                toggle_variable,
+                                flag_name=_OPTION_NAME,
+                                group=35)
+    update_environment()
+    
+    get_workbench().bind("UserWindowAppeared", on_window_appear, True)
