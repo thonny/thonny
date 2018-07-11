53a2d72fa34ba4a2eafd078be9522b3e193fa9fa
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-07-11 13:32:09 +0300

    misc

--- misc/stipple.py
+++ misc/stipple.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import font
 
 root = tk.Tk()
-text = tk.Text(root)
+text = tk.Text(root, spacing3=0)
 text.grid(row=0, column=0, sticky="nsew")
 
 root.columnconfigure(0, weight=1)
@@ -18,18 +18,20 @@ while True:
     elif a == b:
         print("Done")
         break
-    
 print("Done")""")
 
-def get_level_bitmap(width, height, is_first, is_last):
+def get_level_bitmap(width, height, top_bump, bottom_bump):
     
     
-    filename = "temp_%d_%d_%s_%s.xbm" % (width, height, is_first, is_last)
+    filename = "temp_%d_%d_%s_%s.xbm" % (width, height, top_bump, bottom_bump)
     
     #if os.path.exists(filename):
     #    return filename
     
-    offset = 3
+    offset = 5
+    stripe_width = 1
+    bump_height = 1
+    bump_width = 2
     size = width * height
     
     # need to pad size so that it is multiple of 8
@@ -45,15 +47,14 @@ def get_level_bitmap(width, height, is_first, is_last):
             y = i // width
             
             byte <<= 1
-            if (x in [ offset+1] 
-                or is_first and (
-                    y in [0,1] and x == offset+2
-                    or y == 0 and x == offset+3
-                )
-                or is_last and (
-                    y in [height-1,height-2] and x == offset+2
-                    or y == height-1 and x == offset+3
-                )):
+            if (offset <= x < offset+stripe_width 
+                or top_bump 
+                    and y < bump_height
+                    and offset <= x < offset+stripe_width+bump_width
+                or bottom_bump 
+                    and y >= height-bump_height
+                    and offset <= x < offset+stripe_width+bump_width
+                ):
                 byte |= 1
                 
         byte_hexes.append(format(byte, "#04x"))
@@ -74,23 +75,38 @@ def get_level_bitmap(width, height, is_first, is_last):
 
 text_font = font.nametofont(text["font"])
 root.update()
+h = 16
+c = "gray"
 print(text_font.measure("    "), text_font.measure("linespace"))
-text.tag_configure("top", bgstipple="@"+get_level_bitmap(32,16, True, False), background="gray")
-text.tag_configure("middle", bgstipple="@"+get_level_bitmap(32,16, False, False), background="gray")
-text.tag_configure("bottom", bgstipple="@"+get_level_bitmap(32,16, False, True), background="gray")
-text.tag_configure("both", bgstipple="@"+get_level_bitmap(32,16, True, True), background="gray")
+text.tag_configure("top", bgstipple="@"+get_level_bitmap(32,h, True, False), background=c)
+text.tag_configure("middle", bgstipple="@"+get_level_bitmap(32,h, False, False), background=c)
+text.tag_configure("bottom", bgstipple="@"+get_level_bitmap(32,h, False, True), background=c)
+text.tag_configure("both", bgstipple="@"+get_level_bitmap(32,h, True, True), background=c)
 
-text.tag_configure("shift", lmargin1=6)
-text.tag_configure("sel", bgstipple="@white.xbm", background="blue", foreground="white")
+text.tag_configure("shift", lmargin1=8)
 
 text.tag_add("shift", "1.0", "end")
 text.tag_add("top", "2.0", "2.1")
+##############
 
 for i in range(3, 10):
     text.tag_add("middle", "%d.0" % i, "%d.1" % i)
 
 text.tag_add("middle", "10.0", "10.1")
-text.tag_add("top", "11.0", "11.1")
+text.tag_add("both", "11.0", "11.1")
+
+
+#############
+text.tag_add("top", "3.3", "3.5")
+text.tag_add("top", "4.3", "4.5")
+text.tag_add("top", "5.3", "5.5")
+text.tag_add("middle", "6.3", "6.5")
+text.tag_add("middle", "7.3", "7.5")
+text.tag_add("middle", "8.3", "8.5")
+#text.tag_add("middle", "9.3", "4.4")
+text.tag_add("bottom", "9.3", "9.5")
+
+
 text.tag_raise("sel")
 
 
