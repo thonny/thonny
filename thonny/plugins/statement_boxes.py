60188a8c1aa5c1df0c79eeb5a298956ae3fef2a3
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-07-12 18:29:01 +0300

    statement boxes 0.1

--- thonny/plugins/statement_boxes.py
+++ thonny/plugins/statement_boxes.py
@@ -3,6 +3,9 @@ from thonny import get_workbench, jedi_utils
 from tkinter import font
 import thonny
 import logging
+from thonny.codeview import get_syntax_options_for_tag
+
+python_tree = jedi_utils.import_python_tree()
 
 def create_bitmap_file(width, height, predicate, name):
     
@@ -48,28 +51,40 @@ def create_bitmap_file(width, height, predicate, name):
     
 
 
-def configure_tags(text):
-    #text.update()
+def configure_text(text):
+    spacing1 = 2
+    spacing3 = 3
     text_font = text["font"]
+    text.configure(spacing1=spacing1, spacing3=spacing3)
+    text.master._gutter.configure(spacing1=spacing1, spacing3=spacing3)
     if isinstance(text_font, str):
         text_font = font.nametofont(text_font)
     
     indent_width = text_font.measure("    ")
-    _, _, _, line_height = text.bbox("1.0")
-    
-    if line_height < 5:
+    bbox = text.bbox("1.0")
+    if bbox is None or bbox[3] < 5:
         # text not ready yet
+        # TODO: Text in Tk 8.6 has sync method
         return False
+    
+    line_height = bbox[3] + spacing1 + spacing3
     
     print(indent_width, line_height)
     
     def ver(x: int, y: int, top: bool, bottom: bool) -> bool:
         # tells where to show pixels in vertical border of the statement
-        offset = indent_width-5
+        # It would be convenient if tiling started from the start of 
+        # 1st char, but it is offset a bit
+        # In order to make computation easier, I'm offsetting x as well
+        x = (x-5) % indent_width
+        
         stripe_width = 4
-        return (offset <= x < offset+stripe_width 
-                or top and (x >= offset or x <= 2) and y == 0
-                or bottom and (x >= offset or x <= 2) and y == line_height-1
+        gap = 3
+        left = indent_width-stripe_width - gap
+        
+        return (left <= x < left+stripe_width 
+                or top and y == 0 and x >= left
+                or bottom and y == line_height-1 and x >= left
         )
         
     def hor(x: int, y: int, top: bool, bottom: bool) -> bool:
@@ -79,7 +94,7 @@ def configure_tags(text):
             or bottom and y == line_height-1
         )
     
-    color = "red"
+    color = get_syntax_options_for_tag("GUTTER").get("background", "gray")
     for orient, base_predicate in [("ver", ver), ("hor", hor)]:
         for top in [True, False]:
             for bottom in [True, False]:
@@ -94,15 +109,70 @@ def configure_tags(text):
                 bitmap_path = create_bitmap_file(indent_width, line_height, 
                                                  predicate, tag_name)
                 text.tag_configure(tag_name, background=color,
-                                   bgstipple="@" + bitmap_path.replace("\\", "/"))
+                                   bgstipple="@" + bitmap_path)
     
     
     return True
-    
+
+def print_tree(node, level=0):
+    indent = "  " * level
+    #if (isinstance(node, python_tree.PythonNode) and node.type == "sim"
+    if (node.type in ("simple_stmt",)
+        or isinstance(node, python_tree.Flow)
+        ):
+        print(indent, node.type, node.start_pos, node.end_pos)
+        
+    if hasattr(node, "children"):
+        for child in node.children:
+            print_tree(child, level+1)
+
 def add_tags(text):
     source = text.get("1.0", "end")
     tree = jedi_utils.parse_source(source)
-    print("adding tags", tree)
+    
+    print_tree(tree)
+    
+    def tag_tree(node):
+        if (node.type == "simple_stmt"
+            or isinstance(node, (python_tree.Flow,
+                                 python_tree.Scope))):
+            #print(indent, node.type, node.start_pos, node.end_pos)
+            first_line_start = "%d.%d" % node.start_pos
+            first_line_end = "%d.%d" % (node.start_pos[0]+1, 0)
+            print(node)
+            
+            # create horizontal lines
+            if node.start_pos[0] > 1: # skip for first line
+                tag_name = "hor_%s_False" % True # Todo  
+                text.tag_add(tag_name, first_line_start, first_line_end)
+                print(tag_name, first_line_start, first_line_end)
+            
+            if node.start_pos[1] > 0:
+                # tag line beginnings
+                # TODO: handle unprocessed empty and comment lines
+                # between last tag and this one
+                
+                assert node.end_pos[1] == 0 
+                for lineno in range(node.start_pos[0], node.end_pos[0]):
+                    top = lineno == node.start_pos[0]
+                    bottom = False
+                    text.tag_add("ver_%s_%s" % (top, bottom),
+                                 "%d.%d" % (lineno, node.start_pos[1]-2),
+                                 "%d.%d" % (lineno, node.start_pos[1]),
+                                 )
+                    print("ver_%s_%s" % (top, bottom),
+                                 "%d.%d" % (lineno, node.start_pos[1]-2),
+                                 "%d.%d" % (lineno, node.start_pos[1]),
+                                 )
+            
+        if node.type != "simple_stmt" and hasattr(node, "children"):
+            for child in node.children:
+                tag_tree(child)
+    
+    tag_tree(tree)
+
+def handle_editor_event(event):
+    configure_and_add_tags(event.editor.get_text_widget())
 
 def handle_events(event):
     if hasattr(event, "text_widget"):
@@ -115,7 +185,7 @@ def handle_events(event):
 def configure_and_add_tags(text):
     if not getattr(text, "structure_tags_configured", False):
         try:
-            if configure_tags(text):
+            if configure_text(text):
                 text.structure_tags_configured = True
             else:
                 text.after(500, lambda: configure_and_add_tags(text))
@@ -131,4 +201,5 @@ def load_plugin() -> None:
     wb = get_workbench() 
 
     wb.set_default("view.program_structure", False)
-    wb.bind_class("CodeViewText", "<<TextChange>>", handle_events, True)
+    wb.bind("Save", handle_editor_event, True)
+    wb.bind("Open", handle_editor_event, True)
