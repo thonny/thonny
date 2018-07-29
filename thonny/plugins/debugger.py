79c2c396211866c06721a47143073ee8c3710bf3
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-07-29 08:44:56 +0300

    Move StackView to debugger.py

--- thonny/plugins/debugger.py
+++ thonny/plugins/debugger.py
@@ -693,7 +693,29 @@ class FunctionCallDialog(DialogVisualizer):
 class ModuleLoadDialog(DialogVisualizer):
     def __init__(self, text_frame, frame_info):
         DialogVisualizer.__init__(self, text_frame, frame_info)
+
+class StackView(ui_utils.TreeFrame):
+    def __init__(self, master):
+        super().__init__(master, ("function", "location"))
+
+        self.tree.column('function', width=120, anchor=tk.W, stretch=False)
+        self.tree.column('location', width=450, anchor=tk.W, stretch=True)
+        
+        self.tree.heading('function', text='Function', anchor=tk.W) 
+        self.tree.heading('location', text='Location', anchor=tk.W)
+        
+        get_workbench().bind("DebuggerResponse", self._update_stack, True)
+        get_workbench().bind("ToplevelResponse", lambda e=None: self._clear_tree(), True)
     
+    def _update_stack(self, msg):
+        self._clear_tree()
+        for frame in msg.stack:
+            lineno = frame.last_event_focus.lineno
+                
+            node_id = self.tree.insert("", "end")
+            self.tree.set(node_id, "function", frame.code_name)
+            self.tree.set(node_id, "location", 
+                          "%s, line %s" % (frame.filename, lineno))    
 
 def _debugger_command_enabled(command):
     if _current_debugger is None:
@@ -793,4 +815,4 @@ def load_plugin() -> None:
         image="run-to-cursor",
         include_in_toolbar=False)
 
-    
+    get_workbench().add_view(StackView, "Stack", "ne")    
