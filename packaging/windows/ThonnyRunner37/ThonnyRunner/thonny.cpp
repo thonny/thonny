1de75499c8096b372f7988908284ca4dd8b0da23
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-08-13 21:14:39 +0300

    Fixed #472: Confirmation dialog has old style buttons when Thonny is run via thonny.exe

--- packaging/windows/ThonnyRunner37/ThonnyRunner/thonny.cpp
+++ packaging/windows/ThonnyRunner37/ThonnyRunner/thonny.cpp
@@ -1,5 +1,8 @@
 #include <Python.h>
 #include <windows.h>
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
