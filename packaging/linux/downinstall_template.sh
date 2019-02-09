431253a790b0d19a9833314f5ddfe7e7afade1bc
Aivar Annamaa <aivar.annamaa@gmail.com>
2019-02-09 18:13:00 +0200

    Update downinstall_template.sh

--- packaging/linux/downinstall_template.sh
+++ packaging/linux/downinstall_template.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+VER=VERSION 
+
 echo 
-echo "This script will download and install Thonny VERSION for Linux."
+echo "This script will download and install Thonny $VER for Linux (32 or 64-bit PC)."
 read -p "Press ENTER to continue or Ctrl+C to cancel."
 
 ARCHITECTURE="$(uname -m)"
-FILENAME=thonny-VERSION-$ARCHITECTURE.tar.gz
-URL="https://bitbucket.org/plas/thonny/downloads/$FILENAME"
+FILENAME=thonny-$VER-$ARCHITECTURE.tar.gz
+URL="https://github.com/thonny/thonny/releases/download/v$VER/$FILENAME"
 
 echo "Downloading $URL"
 
