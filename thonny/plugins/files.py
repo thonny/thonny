4d702e160b9f1912ba036d67fcdbd7cfc479b092
Aivar Annamaa <aivar.annamaa@cyber.ee>
2019-03-01 09:16:58 +0200

    Progress with new file browser

--- thonny/plugins/files.py
+++ thonny/plugins/files.py
@@ -2,15 +2,12 @@
 
 import os
 import tkinter as tk
-from tkinter import ttk
-from tkinter.messagebox import showerror
 
-# from thonny.ui_utils import askstring TODO: doesn't work
-from tkinter.simpledialog import askstring
-
-from thonny import get_workbench, misc_utils
+from thonny import get_workbench
 from thonny.base_file_browser import BaseFileBrowser
 from thonny.ui_utils import lookup_style_option
+from thonny.common import normpath_with_actual_case
+
 
 class FilesView(tk.PanedWindow):
     def __init__(self, master=None):
@@ -28,50 +25,15 @@ class FilesView(tk.PanedWindow):
 class MainFileBrowser(BaseFileBrowser):
     def __init__(self, master, show_hidden_files=False):
         BaseFileBrowser.__init__(
-            self, master, show_hidden_files, "file.last_browser_folder"
+            self, master, show_hidden_files, 
+            last_folder_setting_name="file.last_browser_folder"
         )
 
-        self.menu = tk.Menu(tk._default_root, tearoff=False)
-        self.menu.add_command(label="Create new file", command=self.create_new_file)
-
-        self.tree.bind("<3>", self.on_secondary_click, True)
-        if misc_utils.running_on_mac_os():
-            self.tree.bind("<2>", self.on_secondary_click, True)
-            self.tree.bind("<Control-1>", self.on_secondary_click, True)
-            
-        self.set_breadcrumbs("C:\\Users\\Aivar\\Documents\\Python\\PyGame\\NikaNaka")
 
     def create_new_file(self):
-        selected_path = self.get_selected_path()
-
-        if not selected_path:
-            return
-
-        if os.path.isdir(selected_path):
-            parent_path = selected_path
-        else:
-            parent_path = os.path.dirname(selected_path)
-
-        initial_name = self.get_proposed_new_file_name(parent_path, ".py")
-        name = askstring(
-            "File name",
-            "Provide filename",
-            initialvalue=initial_name,
-            # selection_range=(0, len(initial_name)-3)
-        )
-
-        if not name:
-            return
-
-        path = os.path.join(parent_path, name)
-
-        if os.path.exists(path):
-            showerror("Error", "The file '" + path + "' already exists", parent=get_workbench())
-        else:
-            open(path, "w").close()
-
-        self.open_path_in_browser(path, True)
-        get_workbench().get_editor_notebook().show_file(path)
+        path = super().create_new_file()
+        if path and path.endswith(".py"):
+            get_workbench().get_editor_notebook().show_file(path)
 
     def get_proposed_new_file_name(self, folder, extension):
         base = "new_file"
@@ -89,31 +51,12 @@ class MainFileBrowser(BaseFileBrowser):
         else:
             return base + extension
 
-    def on_secondary_click(self, event):
-        node_id = self.tree.identify_row(event.y)
-
-        if node_id:
-            self.tree.selection_set(node_id)
-            self.tree.focus(node_id)
-            self.menu.tk_popup(event.x_root, event.y_root)
-
-    def on_double_click(self, event):
-        path = self.get_selected_path()
-        if os.path.isfile(path):
-            get_workbench().get_editor_notebook().show_file(path)
-            self.save_current_folder()
-        elif os.path.isdir(path):
-            self.refresh_tree(self.get_selected_node(), True)
-
-
-
 class RemoteFileBrowser(BaseFileBrowser):
     def __init__(self, master, show_hidden_files=False):
         BaseFileBrowser.__init__(
             self, master, show_hidden_files, "device.last_browser_folder",
             breadcrumbs_pady=(0,7)
         )
-        self.set_breadcrumbs("")
 
     def get_root_text(self):
         return "TARGET DEVICE"
@@ -121,5 +64,7 @@ class RemoteFileBrowser(BaseFileBrowser):
 
 
 def load_plugin() -> None:
-    get_workbench().set_default("file.last_browser_folder", None)
+    get_workbench().set_default("file.last_browser_folder",
+                                normpath_with_actual_case(os.path.expanduser("~")))
+    
     get_workbench().add_view(FilesView, "Files", "nw")
