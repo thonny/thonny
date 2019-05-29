c558e2ec8e64b61ff3573abb9e7c22a1aa28ceb1
André Roberge <andre.roberge@gmail.com>
2019-05-29 19:25:28 -0300

    Code for solving issue-698 implemented.

--- thonny/plugins/theme_and_font_config_page.py
+++ thonny/plugins/theme_and_font_config_page.py
@@ -4,7 +4,7 @@ from tkinter import font as tk_font
 from tkinter import ttk
 
 from thonny import get_workbench
-from thonny.codeview import CodeView
+from thonny.codeview import CodeView, get_syntax_options_for_tag
 from thonny.config_ui import ConfigurationPage
 from thonny.ui_utils import create_string_var
 
@@ -16,6 +16,8 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
         self._original_size = get_workbench().get_option("view.editor_font_size")
         self._original_ui_theme = get_workbench().get_option("view.ui_theme")
         self._original_syntax_theme = get_workbench().get_option("view.syntax_theme")
+        self._original_io_family = get_workbench().get_option("view.io_font_family")
+        self._original_io_size = get_workbench().get_option("view.io_font_size")
 
         ConfigurationPage.__init__(self, master)
 
@@ -34,6 +36,13 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
         self._syntax_theme_variable = create_string_var(
             self._original_syntax_theme, modification_listener=self._update_appearance
         )
+        self._io_family_variable = create_string_var(
+            self._original_io_family, modification_listener=self._update_appearance
+        )
+
+        self._io_size_variable = create_string_var(
+            self._original_io_size, modification_listener=self._update_appearance
+        )
 
         ttk.Label(self, text="UI theme").grid(row=1, column=0, sticky="w", pady=(10, 0))
         self._ui_theme_combo = ttk.Combobox(
@@ -46,9 +55,7 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
         )
         self._ui_theme_combo.grid(row=2, column=0, sticky="nsew", padx=(0, 10))
 
-        ttk.Label(self, text="Syntax theme").grid(
-            row=1, column=1, sticky="w", pady=(10, 0)
-        )
+        ttk.Label(self, text="Syntax theme").grid(row=1, column=1, sticky="w", pady=(10, 0))
         self._syntax_theme_combo = ttk.Combobox(
             self,
             exportselection=False,
@@ -59,9 +66,7 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
         )
         self._syntax_theme_combo.grid(row=2, column=1, sticky="nsew", padx=(0, 10))
 
-        ttk.Label(self, text="Editor font").grid(
-            row=1, column=2, sticky="w", pady=(10, 0)
-        )
+        ttk.Label(self, text="Editor font").grid(row=1, column=2, sticky="w", pady=(10, 0))
         self._family_combo = ttk.Combobox(
             self,
             exportselection=False,
@@ -89,7 +94,7 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
         )
         self._preview_codeview = CodeView(
             self,
-            height=10,
+            height=6,
             font="EditorFont",
             # relief="sunken",
             # borderwidth=1,
@@ -101,26 +106,51 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
             def foo(bar):
                 if bar is None: # This is a comment
                     print("The answer is", 33)
-            
+
             unclosed_string = "blah, blah
             """
             ).strip()
         )
-        self._preview_codeview.grid(
-            row=4, column=0, columnspan=4, sticky=tk.NSEW, pady=(0, 5)
-        )
+        self._preview_codeview.grid(row=4, column=0, columnspan=4, sticky=tk.NSEW, pady=(0, 5))
 
-        (
-            ttk.Label(
-                self, text="NB! Some style elements change only after restarting Thonny"
-            ).grid(row=5, column=0, columnspan=4, sticky="w", pady=(0, 5))
+        ttk.Label(self, text="Shell font").grid(row=5, column=2, sticky="w", pady=(10, 0))
+        self._family_combo = ttk.Combobox(
+            self,
+            exportselection=False,
+            state="readonly",
+            height=15,
+            textvariable=self._io_family_variable,
+            values=self._get_families_to_show(),
+        )
+        self._family_combo.grid(row=6, column=2, sticky=tk.NSEW, padx=(0, 10))
+
+        ttk.Label(self, text="Size").grid(row=5, column=3, sticky="w", pady=(10, 0))
+        self._size_combo = ttk.Combobox(
+            self,
+            width=4,
+            exportselection=False,
+            textvariable=self._io_size_variable,
+            state="readonly",
+            height=15,
+            values=[str(x) for x in range(3, 73)],
+        )
+        self._size_combo.grid(row=6, column=3, sticky="nsew")
+
+        ttk.Label(self, text="Shell preview").grid(
+            row=7, column=0, sticky="w", pady=(10, 0), columnspan=4
+        )
+        self._shell_preview = tk.Text(self, height=3)
+        self._shell_preview.grid(row=8, column=0, columnspan=4, sticky=tk.NSEW, pady=(0, 5))
+        self._insert_shell_text()
+
+        ttk.Label(self, text="NB! Some style elements change only after restarting Thonny").grid(
+            row=9, column=0, columnspan=4, sticky="w", pady=(0, 5)
         )
 
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.columnconfigure(2, weight=1)
         self.columnconfigure(3, weight=1)
-        self.rowconfigure(4, weight=1)
 
     def apply(self):
         # don't do anything, as preview already did the thing
@@ -137,22 +167,39 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
             get_workbench().set_option("view.syntax_theme", self._original_syntax_theme)
             get_workbench().set_option("view.editor_font_size", self._original_size)
             get_workbench().set_option("view.editor_font_family", self._original_family)
+            get_workbench().set_option("view.io_font_size", self._original_io_size)
+            get_workbench().set_option("view.io_font_family", self._original_io_family)
             get_workbench().reload_themes()
             get_workbench().update_fonts()
 
     def _update_appearance(self):
         get_workbench().set_option("view.ui_theme", self._ui_theme_variable.get())
-        get_workbench().set_option(
-            "view.syntax_theme", self._syntax_theme_variable.get()
-        )
-        get_workbench().set_option(
-            "view.editor_font_size", int(self._size_variable.get())
-        )
-        get_workbench().set_option(
-            "view.editor_font_family", self._family_variable.get()
-        )
+        get_workbench().set_option("view.syntax_theme", self._syntax_theme_variable.get())
+        get_workbench().set_option("view.editor_font_size", int(self._size_variable.get()))
+        get_workbench().set_option("view.editor_font_family", self._family_variable.get())
+        get_workbench().set_option("view.io_font_size", int(self._io_size_variable.get()))
+        get_workbench().set_option("view.io_font_family", self._io_family_variable.get())
         get_workbench().reload_themes()
         get_workbench().update_fonts()
+        self._update_shell_appearance()
+
+    def _insert_shell_text(self):
+        self._shell_preview.configure(font="IOFont")
+        self._update_shell_appearance()
+        self._shell_preview.insert("1.0", "Output from a print statement.\n", "stdout")
+        self._shell_preview.insert(tk.END, "Input from a user.\n", "stdin")
+        self._shell_preview.insert(tk.END, "A Python exception.", "stderr")
+
+    def _update_shell_appearance(self):
+        background = get_syntax_options_for_tag("TEXT")["background"]
+        stderr_fg = get_syntax_options_for_tag("stderr")["foreground"]
+        stdin_fg = get_syntax_options_for_tag("stdin")["foreground"]
+        stdout_fg = get_syntax_options_for_tag("stdout")["foreground"]
+
+        self._shell_preview.configure(background=background)
+        self._shell_preview.tag_configure("stderr", foreground=stderr_fg)
+        self._shell_preview.tag_configure("stdin", foreground=stdin_fg)
+        self._shell_preview.tag_configure("stdout", foreground=stdout_fg)
 
     def _get_families_to_show(self):
         # In Linux, families may contain duplicates (actually different fonts get same names)
@@ -160,6 +207,4 @@ class ThemeAndFontConfigurationPage(ConfigurationPage):
 
 
 def load_plugin() -> None:
-    get_workbench().add_configuration_page(
-        "Theme & Font", ThemeAndFontConfigurationPage
-    )
+    get_workbench().add_configuration_page("Theme & Font", ThemeAndFontConfigurationPage)
