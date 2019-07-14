7cbb6306c6e57ec626004a102a8470c19dd2a501
Aivar Annamaa <aivar.annamaa@gmail.com>
2019-07-14 19:09:50 +0300

    translation helper scripts

---
+++ thonny/locale/compile_mo.bat
@@ -0,0 +1,3 @@
+cd ..\..
+C:\Py3\Scripts\pybabel compile -d thonny/locale/ -D thonny
+pause
--- thonny/locale/update_pot.bat
+++ thonny/locale/update_pot.bat
@@ -1,2 +1,3 @@
+cd ..\..
 C:\Py3\Scripts\pybabel extract thonny/ --output-file thonny/locale/thonny.pot
 pause
