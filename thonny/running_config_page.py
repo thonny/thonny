thonny/thonny

commit bdc1fa5bd0f7b908eeb509c1106039dbd87992d2

Make simple backend configuration pages more robust

thonny/running_config_page.py
@@ -8,8 +8,28 @@ from thonny import get_workbench, running, ui_utils
 from thonny.common import normpath_with_actual_case
 from thonny.misc_utils import running_on_mac_os, running_on_windows
 from thonny.plugins.backend_config_page import BackendDetailsConfigPage
-from thonny.running import WINDOWS_EXE
+from thonny.running import WINDOWS_EXE, get_interpreter_for_subprocess
 from thonny.ui_utils import SubprocessDialog, askdirectory, askopenfilename, create_string_var
+
+
+class SameAsFrontEndConfigurationPage(BackendDetailsConfigPage):
+    def __init__(self, master):
+        super().__init__(master)
+        label = ttk.Label(self, text=get_interpreter_for_subprocess())
+        label.grid()
+
+
+class PrivateVenvConfigurationPage(BackendDetailsConfigPage):
+    def __init__(self, master):
+        super().__init__(master)
+        text = (
+            _("This virtual environment is automatically maintained by Thonny.\n")
+            + _("Location: ")
+            + running.get_private_venv_path()
+        )
+
+        label = ttk.Label(self, text=text)
+        label.grid()
 
 
 class CustomCPythonConfigurationPage(BackendDetailsConfigPage):

thonny/workbench.py
@@ -507,15 +507,15 @@ class Workbench(tk.Tk):
         self.set_default("CustomInterpreter.used_paths", [])
         self.set_default("CustomInterpreter.path", "")
 
+        from thonny import running_config_page
+
         self.add_backend(
             "SameAsFrontend",
             running.SameAsFrontendCPythonProxy,
             _("The same interpreter which runs Thonny (default)"),
-            running.get_interpreter_for_subprocess(),
+            running_config_page.SameAsFrontEndConfigurationPage,
             "1",
         )
-
-        from thonny import running_config_page
 
         self.add_backend(
             "CustomCPython",
@@ -529,9 +529,7 @@ class Workbench(tk.Tk):
             "PrivateVenv",
             running.PrivateVenvCPythonProxy,
             _("A special virtual environment (deprecated)"),
-            _("This virtual environment is automatically maintained by Thonny.\n")
-            + _("Location: ")
-            + running.get_private_venv_path(),
+            running_config_page.PrivateVenvConfigurationPage,
             "z",
         )
 
@@ -1086,9 +1084,8 @@ class Workbench(tk.Tk):
 
         # assing names to related classes
         proxy_class.backend_name = name  # type: ignore
-        if not isinstance(config_page_constructor, str):
-            if not getattr(config_page_constructor, "backend_name", None):
-                config_page_constructor.backend_name = name
+        if not getattr(config_page_constructor, "backend_name", None):
+            config_page_constructor.backend_name = name
 
     def add_ui_theme(
         self,

commit 99fa03716091f57cb71c4cbb6162fda2224f3089

Fix backend config page regression from bdc1fa5bd0f7b908eeb509c1106039dbd87992d2

thonny/running_config_page.py
@@ -18,6 +18,9 @@ class SameAsFrontEndConfigurationPage(BackendDetailsConfigPage):
         label = ttk.Label(self, text=get_interpreter_for_subprocess())
         label.grid()
 
+    def should_restart(self):
+        return False
+
 
 class PrivateVenvConfigurationPage(BackendDetailsConfigPage):
     def __init__(self, master):
@@ -31,6 +34,9 @@ class PrivateVenvConfigurationPage(BackendDetailsConfigPage):
         label = ttk.Label(self, text=text)
         label.grid()
 
+
+    def should_restart(self):
+        return False
 
 class CustomCPythonConfigurationPage(BackendDetailsConfigPage):
     def __init__(self, master):