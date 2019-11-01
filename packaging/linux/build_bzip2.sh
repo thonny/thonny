4d4190f59e4c4f864c11c62eb3c2edd64d632aae
Aivar Annamaa <aivar.annamaa@gmail.com>
2019-11-01 09:03:03 +0200

    Update dependencies

--- packaging/linux/build_bzip2.sh
+++ packaging/linux/build_bzip2.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BZIP2_VERSION=1.0.6
+BZIP2_VERSION=1.0.8
 # https://lwn.net/Articles/762264/
 # wget http://www.bzip.org/1.0.6/bzip2-$BZIP2_VERSION.tar.gz
 wget https://fossies.org/linux/misc/bzip2-$BZIP2_VERSION.tar.gz
