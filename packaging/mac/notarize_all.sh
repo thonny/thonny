3829307e487f90c56a8a05e7415b77313e73630d
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-02-18 09:08:29 +0200

    Install to USB instead of portable, #79

--- packaging/mac/notarize_all.sh
+++ packaging/mac/notarize_all.sh
@@ -6,7 +6,6 @@ thonny_version=$(<../../thonny/VERSION)
 thonny_version="3.2.6"
 
 ~/private/notarize-app.sh "dist/thonny-${thonny_version}-macos-installer.pkg"
-~/private/notarize-app.sh "dist/thonny-${thonny_version}-macos-portable.dmg"
 ~/private/notarize-app.sh "dist/thonny-xxl-${thonny_version}-macos-installer.pkg"
 
 
--- packaging/mac/staple_all.sh
+++ packaging/mac/staple_all.sh
@@ -6,5 +6,4 @@ thonny_version=$(<../../thonny/VERSION)
 thonny_version="3.2.6"
 
 ~/private/staple.sh "dist/thonny-${thonny_version}-macos-installer.pkg"
-~/private/staple.sh "dist/thonny-${thonny_version}-macos-portable.dmg"
 ~/private/staple.sh "dist/thonny-xxl-${thonny_version}-macos-installer.pkg"
