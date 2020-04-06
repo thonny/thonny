0444747853be66c240daed091ba1f3268da4d571
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-04-06 22:08:33 +0300

    More macOS entitlements (like official Python), #1115

--- packaging/mac/create_release.sh
+++ packaging/mac/create_release.sh
@@ -17,8 +17,8 @@ echo "### Creating XXL installer ###############################################
 ./sign_bundle_in_build.sh
 ./create_installer_from_build.sh "thonny-xxl" "Thonny XXL" $thonny_version
 
-echo 
-echo "### Creating portable DMG ####################################################"
+#echo 
+#echo "### Creating portable DMG ####################################################"
 #./prepare_dist_bundle.sh $thonny_version ../requirements-regular-bundle.txt
 #cp ../portable_thonny.ini build/Thonny.app/Contents/Frameworks/Python.framework/Versions/3.7/bin
 #./sign_bundle_in_build.sh
