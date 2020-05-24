9d8fcd1add6b702414d5efb3fc6a8a9cc9cfece0
Andrew Scheller <github@loowis.durge.org>
2020-05-24 21:39:09 +0100

    Bash install - check architecture before downloading (#1202)
    Fixes #1145

--- packaging/linux/downinstall_template.sh
+++ packaging/linux/downinstall_template.sh
@@ -4,11 +4,16 @@ set -e
 VERSION=_VERSION_
 VARIANT=_VARIANT_
 
+ARCHITECTURE="$(uname -m)"
+if [[ "$ARCHITECTURE" != "i686" ]] && [[ "$ARCHITECTURE" != "x86_64" ]]; then
+  echo "Thonny only provides pre-built bundles for i686 and x86_64 (not $ARCHITECTURE)"
+  exit 1
+fi
+
 echo 
 echo "This script will download and install Thonny ($VARIANT-$VERSION) for Linux (32 or 64-bit PC)."
 read -p "Press ENTER to continue or Ctrl+C to cancel."
 
-ARCHITECTURE="$(uname -m)"
 FILENAME=$VARIANT-$VERSION-$ARCHITECTURE.tar.gz
 URL="https://github.com/thonny/thonny/releases/download/v$VERSION/$FILENAME"
 
