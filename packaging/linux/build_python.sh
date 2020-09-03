cdbef6fe0387a8e3924095e96e29148a1983ef98
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-09-03 17:27:45 +0300

    Upgrade Python in Linux build

--- packaging/linux/build_python.sh
+++ packaging/linux/build_python.sh
@@ -4,7 +4,7 @@ set -e
 
 # COMPILE PYTHON ###############################################
 
-PYTHON_VERSION=3.7.8
+PYTHON_VERSION=3.7.9
 RELEASE_NAME=Python-${PYTHON_VERSION}
 
 wget https://www.python.org/ftp/python/$PYTHON_VERSION/$RELEASE_NAME.tar.xz
