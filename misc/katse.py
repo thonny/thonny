5f831f257904db449d43232785ce71af682d53c3
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-09-22 14:26:42 +0300

    experiments

--- misc/katse.py
+++ misc/katse.py
@@ -4,13 +4,18 @@ from tkinter import ttk
 root = tk.Tk()
 
 entry = ttk.Entry(root)
-entry.grid()
+#entry.grid()
 
 combo = ttk.Combobox(root)
-combo.grid()
+#combo.grid()
 
 from tkinter.font import nametofont
 
 nametofont("TkTextFont").configure(size=33)
+
+def doit(event):
+    print(event.state)
+
+root.bind_all("<Control-S>", doit, True)
 
 root.mainloop()
--- misc/mmm.py
+++ misc/mmm.py
@@ -1,3 +1,28 @@
-import sys
-print(sys.path)
+import time
+
+import serial
+
+code = """
+s = %r
+print()
+print(len(s), set(s))
+""" % ("*" * 1850)
+
+#s = serial.Serial("/dev/ttyUSB0", 115200)
+s = serial.Serial("/dev/ttyACM0", 115200)
+
+s.write(b"\x03")
+time.sleep(0.2)
+s.write(b"\x03")
+s.write(b"\x01")
+
+print(s.read_until(b">"))
+s.write(code.encode())
+s.flush()
+time.sleep(0.1)
+s.write(b"\x04")
+
+while True:
+    print(s.read(1).decode(), end="")
+
 
