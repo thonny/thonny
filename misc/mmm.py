9f35337deb99ced9189f9f43987ed2f837459b25
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-09-22 15:28:58 +0300

    Update mmm.py

--- misc/mmm.py
+++ misc/mmm.py
@@ -2,15 +2,15 @@ import time
 
 import serial
 
-count = 370
+count = 1870
 code = """
 s = %r
 print()
 print(len(s), set(s))
 """ % ("*" * count)
 
-#s = serial.Serial("/dev/ttyUSB0", 115200)
-s = serial.Serial("COM4", 115200)
+s = serial.Serial("/dev/ttyUSB0", 115200)
+#s = serial.Serial("COM4", 115200)
 
 s.write(b"\x03")
 time.sleep(0.2)
@@ -25,6 +25,7 @@ for i in range(10):
     s.flush()
     s.write(b"\x04")
     print(s.read_until(b"OK"))
+    print(s.read_until(b">"))
 
 while True:
     print(s.read(1).decode(), end="")
