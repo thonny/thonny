741604a16835b7729a80761c4a14423514c43992
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-09-22 15:25:46 +0300

    Enhanced rate tester

--- misc/mmm.py
+++ misc/mmm.py
@@ -2,25 +2,29 @@ import time
 
 import serial
 
+count = 370
 code = """
 s = %r
 print()
 print(len(s), set(s))
-""" % ("*" * 1850)
+""" % ("*" * count)
 
 #s = serial.Serial("/dev/ttyUSB0", 115200)
-s = serial.Serial("/dev/ttyACM0", 115200)
+s = serial.Serial("COM4", 115200)
 
 s.write(b"\x03")
 time.sleep(0.2)
 s.write(b"\x03")
 s.write(b"\x01")
 
-print(s.read_until(b">"))
-s.write(code.encode())
-s.flush()
-time.sleep(0.1)
-s.write(b"\x04")
+print(s.read_until(b"exit\r\n>"))
+print("here we go")
+
+for i in range(10):
+    s.write(code.encode("UTF-8"))
+    s.flush()
+    s.write(b"\x04")
+    print(s.read_until(b"OK"))
 
 while True:
     print(s.read(1).decode(), end="")
