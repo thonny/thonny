46f80c654f0171349e43f526698e9bae9424492e
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-10-18 15:04:13 +0300

    Collect info about CP devices, #1375

--- thonny/plugins/circuitpython/__init__.py
+++ thonny/plugins/circuitpython/__init__.py
@@ -18,31 +18,54 @@ from thonny.ui_utils import askopenfilename, create_url_label, show_dialog
 class CircuitPythonProxy(BareMetalMicroPythonProxy):
     @classmethod
     def get_known_usb_vids_pids(cls):
-        """Copied from https://github.com/mu-editor/mu/blob/master/mu/modes/circuitpython.py"""
+        """Information gathered from
+        https://github.com/mu-editor/mu/blob/master/mu/modes/circuitpython.py
+        https://github.com/microsoft/uf2-samdx1
+        """
         return {
-            (0x239A, None),  # Adafruit
-            (0x2B04, 0xC00C),  # Particle Argon
-            (0x2B04, 0xC00D),  # Particle Boron
-            (0x2B04, 0xC00E),  # Particle Xenon
+            (0x03EB, 0x2402),  # Generic Corp., SAMD21 or SAME54 Board
+
+            (0x04D8, 0xEC72),  # XinaBox CC03
+            (0x04D8, 0xEC75),  # XinaBox CS11
+            (0x04D8, 0xED94),  # PyCubed
+            (0x04D8, 0xED5E),  # XinaBox CW03
+            (0x04D8, 0xEDB3),  # Capable Robot Components, Programmable USB Hub
+            (0x04D8, 0xEDBE),  # maholli, SAM32
+            (0x04D8, 0xEF66),  # eduSense, senseBox MCU
+
+            (0x1209, 0x2017),  # Benjamin Shockley, Mini SAM M4
+            (0x1209, 0x4D44),  # Robotics Masters, Robo HAT MM1 M4
+            (0x1209, 0x7102),  # Mini SAM M0
             (0x1209, 0xBAB1),  # Electronic Cats Meow Meow
             (0x1209, 0xBAB2),  # Electronic Cats CatWAN USBStick
             (0x1209, 0xBAB3),  # Electronic Cats Bast Pro Mini M0
             (0x1209, 0xBAB6),  # Electronic Cats Escornabot Makech
+
+            (0x16D0, 0x0CDA),  # dadamachines, automat
+
+            (0x1B4F, 0x0016),  # Sparkfun Thing Plus - SAMD51
             (0x1B4F, 0x8D22),  # SparkFun SAMD21 Mini Breakout
             (0x1B4F, 0x8D23),  # SparkFun SAMD21 Dev Breakout
-            (0x1209, 0x2017),  # Mini SAM M4
-            (0x1209, 0x7102),  # Mini SAM M0
-            (0x04D8, 0xEC72),  # XinaBox CC03
-            (0x04D8, 0xEC75),  # XinaBox CS11
-            (0x04D8, 0xED5E),  # XinaBox CW03
-            (0x3171, 0x0101),  # 8086.net Commander
-            (0x04D8, 0xED94),  # PyCubed
-            (0x04D8, 0xEDBE),  # SAM32
+
             (0x1D50, 0x60E8),  # PewPew Game Console
-            (0x2886, 0x802D),  # Seeed Wio Terminal
-            (0x1B4F, 0x0016),  # Sparkfun Thing Plus - SAMD51
+            (0x1D50, 0x6110),  # Eitech, Robotics
+            (0x1D50, 0x6112),  # Watterott electronic, Wattuino RC
+
+            (0x2341, 0x8053),  # Arduino LLC, Arduino MKR1300
             (0x2341, 0x8057),  # Arduino Nano 33 IoT board
+
+            (0x239A, None),    # Adafruit
+
+            (0x2886, 0x802D),  # Seeed Wio Terminal
+            (0x2886, 0x000D),  # Seeed Studio, Grove Zero
+
+            (0x2B04, 0xC00C),  # Particle Argon
+            (0x2B04, 0xC00D),  # Particle Boron
+            (0x2B04, 0xC00E),  # Particle Xenon
+
+            (0x3171, 0x0101),  # 8086.net Commander
         }
+
 
     @classmethod
     def get_vids_pids_to_avoid(self):
