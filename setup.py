430c32ac3d0fea02ebce52ba0852028444930c12
Aivar Annamaa <aivar.annamaa@gmail.com>
2020-11-15 12:31:08 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Education",
         "Topic :: Software Development",
         "Topic :: Software Development :: Debuggers",
