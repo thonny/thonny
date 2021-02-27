50b8d88799bf4eec1b751106f83f1a12807e0abc
Aivar Annamaa <aivar.annamaa@gmail.com>
2021-02-27 15:18:09 +0200

    Update Linux bundle, #870

--- packaging/linux/build_tcltk.sh
+++ packaging/linux/build_tcltk.sh
@@ -2,18 +2,19 @@
 
 set -e
 
+VERSION=8.6.11
+
 # COMPILE TCL/TK ####################################################
-# wget http://prdownloads.sourceforge.net/tcl/tcl8.6.8-src.tar.gz
-VERSION=8.6.10
-wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl$VERSION-src.tar.gz
+wget https://downloads.sourceforge.net/project/tcl/Tcl/${VERSION}/tcl${VERSION}-src.tar.gz
+# wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl$VERSION-src.tar.gz
 tar -xzf tcl$VERSION-src.tar.gz
 cd tcl$VERSION/unix
 ./configure --prefix=$PREFIX
 make install
 cd ../..
 
-# wget http://prdownloads.sourceforge.net/tcl/tk8.6.8-src.tar.gz
-wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk$VERSION-src.tar.gz
+wget https://downloads.sourceforge.net/project/tcl/Tcl/${VERSION}/tk${VERSION}.1-src.tar.gz
+# wget ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk$VERSION-src.tar.gz
 tar -xzf tk$VERSION-src.tar.gz
 cd tk$VERSION/unix
 # see http://sourceforge.net/p/tktoolkit/bugs/2588/ for --disable-xss
