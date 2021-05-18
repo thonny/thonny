0b8c049135106932dc9cf71ddc6e91edf8bf1159
Aivar Annamaa <aivar.annamaa@gmail.com>
2021-05-18 14:22:52 +0300

    Update create_tarball.sh

--- packaging/linux/create_tarball.sh
+++ packaging/linux/create_tarball.sh
@@ -27,10 +27,13 @@ export LD_LIBRARY_PATH=$TARGET_DIR/lib
 
 # INSTALL DEPS ###################################
 
-#if [ `getconf LONG_BIT` = "32" ]
-#then
-#    $TARGET_DIR/bin/python3.7 -s -m pip install setuptools-scm
-#fi
+if [ `getconf LONG_BIT` = "32" ]
+then
+#    $TARGET_DIR/bin/python3.9 -s -m pip install setuptools-scm
+
+    # newer cryptography versions can't be (easily?) built on Ubuntu 16.04
+    $TARGET_DIR/bin/python3.9 -s -m pip install cryptography==3.2.*
+fi
 
 $TARGET_DIR/bin/python3.7 -s -m pip install --no-cache-dir wheel
 $TARGET_DIR/bin/python3.7 -s -m pip install --no-cache-dir --no-binary mypy -r ../requirements-regular-bundle.txt
