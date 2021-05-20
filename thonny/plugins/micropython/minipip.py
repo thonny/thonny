d2cf9ed962edd2d305c168271be0c0199d523b80
Aivar Annamaa <aivar.annamaa@gmail.com>
2021-05-20 13:08:00 +0300

    Update minipip.py

--- thonny/plugins/micropython/minipip.py
+++ thonny/plugins/micropython/minipip.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+"""
+MIT License
+
+Copyright (c) 2021 Aivar Annamaa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import io
 import json
@@ -22,7 +45,8 @@ except ImportError:
     # before Python 3.8
     def shlex_join(split_command):
         """Return a shell-escaped string from *split_command*."""
-        return ' '.join(shlex.quote(arg) for arg in split_command)
+        return " ".join(shlex.quote(arg) for arg in split_command)
+
 
 from pkg_resources import Requirement
 
@@ -32,7 +56,7 @@ MP_ORG_INDEX = "https://micropython.org/pi"
 PYPI_INDEX = "https://pypi.org/pypi"
 DEFAULT_INDEX_URLS = [MP_ORG_INDEX, PYPI_INDEX]
 
-__version__ = "0.1b1"
+__version__ = "0.1b2"
 
 
 class UserError(RuntimeError):
@@ -336,6 +360,15 @@ def _resolve_version(req: Requirement, main_meta: Dict[str, Any]) -> Optional[st
     return sorted(matching_versions, key=pkg_resources.parse_version)[-1]
 
 
+def error(msg):
+    if sys.stderr.isatty():
+        print("\x1b[31m", msg, "\x1b[0m", sep="", file=sys.stderr)
+    else:
+        print(msg, file=sys.stderr)
+
+    return 1
+
+
 def main(raw_args: Optional[List[str]] = None) -> int:
     if raw_args is None:
         raw_args = sys.argv[1:]
@@ -357,7 +390,7 @@ def main(raw_args: Optional[List[str]] = None) -> int:
     parser.add_argument(
         "specs",
         help="Package specification, eg. 'micropython-os' or 'micropython-os>=0.6'",
-        nargs="+",
+        nargs="*",
         metavar="package_spec",
     )
     parser.add_argument(
@@ -372,7 +405,8 @@ def main(raw_args: Optional[List[str]] = None) -> int:
     parser.add_argument(
         "-p",
         "--port",
-        help="Serial port of the device",
+        help="Serial port of the device "
+        "(specify if you want minipip to upload the result to the device)",
         nargs="?",
     )
     parser.add_argument(
@@ -402,9 +436,7 @@ def main(raw_args: Optional[List[str]] = None) -> int:
         action="store_true",
     )
     parser.add_argument(
-        "--version",
-        help="Show program version and exit",
-        action="store_true",
+        "--version", help="Show program version and exit", action="version", version=__version__
     )
     args = parser.parse_args(args=raw_args)
 
@@ -435,25 +467,18 @@ def main(raw_args: Optional[List[str]] = None) -> int:
     logger.addHandler(console_handler)
 
     if args.port and not _get_rshell_command():
-        print(
-            "Could not find rshell (required for uploading when serial port is given)",
-            file=sys.stderr,
-        )
-        return 1
+        return error("Could not find rshell (required for uploading when serial port is given)")
 
     if args.port and not args.target_dir.startswith("/"):
-        print("If port is given then target dir must be absolute Unix-style path")
-        return 1
+        return error("If port is given then target dir must be absolute Unix-style path")
 
-    if args.version:
-        print(__version__)
-        return 0
+    if not all_specs:
+        return error("At least one package specifier or non-empty requirements file is required")
 
     try:
         install(all_specs, target_dir=args.target_dir, index_urls=index_urls, port=args.port)
     except UserError as e:
-        print("ERROR:", e, file=sys.stderr)
-        return 1
+        return error(str(e))
     except subprocess.CalledProcessError:
         # assuming the subprocess (pip or rshell) already printed the error
         return 1
