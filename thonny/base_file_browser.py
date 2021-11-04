eee95ea5a91b9b7cff2c13cfa6e9a58b62735553
kr-g <k.r.goger@gmail.com>
2021-11-04 09:23:14 +0100

    forgot to format with black

--- thonny/base_file_browser.py
+++ thonny/base_file_browser.py
@@ -715,10 +715,14 @@ class BaseFileBrowser(ttk.Frame):
         if self.supports_copypaste():
             self.menu.add_command(label=tr("Copy"), command=self.copy_files)
             self.menu.add_command(label=tr("Cut"), command=self.cut_files)
-            target = self.get_selected_file()            
+            target = self.get_selected_file()
             self.menu.add_command(label=tr("Paste"), command=self.paste_files)
-            if target is None or not self.copypaste.has_selection() or self.copypaste.conflicts(target):
-                self.menu.entryconfig(tr("Paste"), state = "disabled")
+            if (
+                target is None
+                or not self.copypaste.has_selection()
+                or self.copypaste.conflicts(target)
+            ):
+                self.menu.entryconfig(tr("Paste"), state="disabled")
 
     def add_last_menu_items(self, context):
         self.menu.add_command(label=tr("Properties"), command=self.show_properties)
