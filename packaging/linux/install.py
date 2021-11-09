c55d05937f10aa32c14b68222e72abb9059415bd
Aivar Annamaa <aivar.annamaa@gmail.com>
2021-11-09 15:35:52 +0200

    Update Linux installer

--- packaging/linux/install.py
+++ packaging/linux/install.py
@@ -172,7 +172,7 @@ try:
 
     print_task("Compiling Python files")
     return_code = subprocess.call(
-        [target_dir + "/bin/python3.9", "-m", "compileall", target_dir + "/lib"]
+        [target_dir + "/bin/python3.10", "-m", "compileall", target_dir + "/lib"]
     )
     # TODO: why is return code 1 (eg. in 64-bit Fedora 22) even when everything seemed to succeed?
     print("Done!")
