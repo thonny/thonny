5453b61b2ad756ff95d746767e5a18ac7048ef23
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-04-09 22:23:40 +0300

    update win installer scripts

--- packaging/windows/create_installer_38.bat
+++ packaging/windows/create_installer_38.bat
@@ -16,12 +16,13 @@ copy thonny_python.ini %BUILDDIR%
 
 @echo ............... INSTALLING DEPS ...................................
 
-%BUILDDIR%\python -s -m pip install --no-cache-dir wheel
+%BUILDDIR%\python -s -m pip install --no-warn-script-location wheel
 
-%BUILDDIR%\python -s -m pip install --no-cache-dir --no-binary mypy -r ..\requirements-regular-bundle.txt
+%BUILDDIR%\python -s -m pip install --no-warn-script-location --no-binary mypy -r ..\requirements-regular-bundle.txt
 
 @echo ............... INSTALLING THONNY ...................................
-%BUILDDIR%\python -s -m pip install --pre --no-cache-dir thonny
+%BUILDDIR%\python -s -m pip install --no-warn-script-location --pre --no-cache-dir thonny
+@rem %BUILDDIR%\python -s -m pip install --no-warn-script-location ..\setuptools\thonny-4.0.0b2.dev0-py3-none-any.whl
 
 @echo ............... CLEANING PYTHON ............................
 @rem move following 3 files to avoid confusion (user may think they're Thonny license etc.)
