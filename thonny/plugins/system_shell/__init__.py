2cb7201c0a87925534896b204c6318be3b52d81e
kr-g <k.r.goger+github@gmail.com>
2022-08-07 12:14:27 +0200

    shortkey for opening terminal

--- thonny/plugins/system_shell/__init__.py
+++ thonny/plugins/system_shell/__init__.py
@@ -8,6 +8,7 @@ from thonny.common import get_augmented_system_path, get_exe_dirs
 from thonny.languages import tr
 from thonny.misc_utils import inside_flatpak, show_command_not_available_in_flatpak_message
 from thonny.running import get_environment_overrides_for_python_subprocess
+from thonny.ui_utils import select_sequence
 
 
 def _open_system_shell():
@@ -64,4 +65,5 @@ def load_plugin() -> None:
         _open_system_shell,
         group=80,
         image="terminal",
+        default_sequence=select_sequence("<Shift-Control-T>", "<Shift-Command-T>"),
     )
