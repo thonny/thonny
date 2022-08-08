4cd22f364573030194145e0cb841a821bb386122
kr-g <k.r.goger+github@gmail.com>
2022-08-08 12:08:50 +0200

    changed regex to multiline

--- thonny/plugins/todo_view.py
+++ thonny/plugins/todo_view.py
@@ -69,8 +69,8 @@ class TodoView(ui_utils.TreeFrame):
 
         # todo support of other file types and introducing comment tags
 
-        r_ex = r"((#\s*(TODO|BUG|FIXME|ERROR|NOTE|REMARK)[:\s]*).*)"
-        r_match = re.compile(r_ex, re.IGNORECASE)
+        r_ex = r"^.*((#[\t ]*(TODO|BUG|FIXME|ERROR|NOTE|REMARK)([:\t ]*))(.*))$"
+        r_match = re.compile(r_ex, re.IGNORECASE | re.MULTILINE)
 
         line_no = 0
         for line in new_source.splitlines():
