4e43a9d9356a0dd4b605a931143ae2a6274af715
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-08-12 19:47:31 +0300

    New flashers for RP2040, CircuitPython, micro:bit, #2344

--- thonny/plugins/rp2040/__init__.py
+++ thonny/plugins/rp2040/__init__.py
@@ -1,5 +1,8 @@
+import os.path
 from logging import getLogger
 
+from thonny import ui_utils
+from thonny.misc_utils import list_volumes
 from thonny.plugins.micropython import add_micropython_backend
 from thonny.plugins.micropython.mp_common import RAW_PASTE_SUBMIT_MODE
 from thonny.plugins.micropython.mp_front import (
@@ -26,9 +29,15 @@ class RP2040BackendProxy(BareMetalMicroPythonProxy):
 
     @classmethod
     def device_is_present_in_bootloader_mode(cls):
-        targets = Uf2FlashingDialog.get_possible_targets("RPI-RP2")
-        logger.info("Bootloader targets: %r", targets)
-        return bool(targets)
+        for vol in list_volumes(skip_letters=["A"]):
+            info_path = os.path.join(vol, "INFO_UF2.TXT")
+            if os.path.isfile(info_path):
+                with open(info_path, encoding="utf-8", errors="replace") as fp:
+                    for line in fp:
+                        if line.startswith("Board-ID: RPI-RP2"):
+                            return True
+
+        return False
 
     def get_node_label(self):
         return "RP2040 device"
@@ -45,7 +54,11 @@ class RP2040BackendProxy(BareMetalMicroPythonProxy):
 
 class RP2040BackendConfigPage(BareMetalMicroPythonConfigPage):
     def _has_flashing_dialog(self):
-        return False
+        return True
+
+    def _open_flashing_dialog(self):
+        dlg = Uf2FlashingDialog(self, firmware_name="MicroPython")
+        ui_utils.show_dialog(dlg)
 
 
 def load_plugin():
