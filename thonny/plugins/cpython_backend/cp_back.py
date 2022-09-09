7a5b5d82b3e25ef648ef0c3bbd355131da9be41f
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-09-09 13:02:57 +0300

    Use plainpager for help, #2435

--- thonny/plugins/cpython_backend/cp_back.py
+++ thonny/plugins/cpython_backend/cp_back.py
@@ -79,7 +79,7 @@ class MainCPythonBackend(MainBackend):
         self._main_dir = os.path.dirname(sys.modules["thonny"].__file__)
         self._heap = {}  # WeakValueDictionary would be better, but can't store reference to None
         self._source_info_by_frame = {}
-        site.sethelper()  # otherwise help function is not available
+        self._init_help()
         self._install_fake_streams()
         self._install_repl_helper()
         self._current_executor = None
@@ -792,6 +792,13 @@ class MainCPythonBackend(MainBackend):
                 builtins._ = obj
 
         setattr(builtins, _REPL_HELPER_NAME, _handle_repl_value)
+
+    def _init_help(self):
+        import pydoc
+
+        pydoc.pager = pydoc.plainpager
+        site.sethelper()  # otherwise help function is not available
+
 
     def _install_fake_streams(self):
         self._original_stdin = sys.stdin
