4a432c5f38a527dd8a619a9fe666f3f36526e6b3
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-09-09 14:08:13 +0300

    Clearer call

--- thonny/ui_utils.py
+++ thonny/ui_utils.py
@@ -535,7 +535,7 @@ class ClosableNotebook(ttk.Notebook):
         self.close_tab(self._popup_index)
 
     def _close_other_tabs(self):
-        self.close_tabs(self._popup_index)
+        self.close_tabs(except_index=self._popup_index)
 
     def close_tabs(self, except_index=None):
         for tab_index in reversed(range(len(self.winfo_children()))):
