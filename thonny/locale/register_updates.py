36ac8c63a7f5838f5482a98fe0232a5e95e4b236
Aivar Annamaa <aivar.annamaa@gmail.com>
2023-01-01 16:46:04 +0200

    Update several translations

--- thonny/locale/register_updates.py
+++ thonny/locale/register_updates.py
@@ -22,23 +22,26 @@ def register_locale(name: str) -> None:
 
     new_registered = {}
 
-    new_messages = []
+    review_messages = []
 
     for entry in po:
-        if entry.msgid not in registered or registered[entry.msgid] != entry.msgstr:
+        if entry.msgstr and (entry.msgid not in registered
+                             or registered[entry.msgid] != entry.msgstr):
+
             msg = entry.msgstr.strip().replace("\n", " ")
             if not msg.endswith("."):
                 msg = msg + "."
-            new_messages.append(msg)
+
+            review_messages.append(msg)
 
         if entry.msgstr:
             new_registered[entry.msgid] = entry.msgstr
 
-    if new_messages:
-        print("\n".join(new_messages))
-        pyperclip.copy("\n".join(new_messages))
+    if review_messages:
+        print("\n".join(review_messages))
+        pyperclip.copy("\n".join(review_messages))
 
-        input("... Press ENTER to confirm! ...")
+        input(f"... Press ENTER to confirm {name}! ...")
 
     with open(registered_path, "w", encoding="utf-8") as fp:
         json.dump(new_registered, fp, sort_keys=True, indent=4, ensure_ascii=False)
