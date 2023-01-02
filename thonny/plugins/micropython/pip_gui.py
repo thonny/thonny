f851b329fa9c730824c24e77ff792d25dfc6dbc3
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-01-02 21:54:06 +0100

    more strings are translatable

--- thonny/plugins/micropython/pip_gui.py
+++ thonny/plugins/micropython/pip_gui.py
@@ -86,15 +86,15 @@ class MicroPythonPipDialog(BackendPipDialog):
 
     def _show_extra_instructions(self):
         self._append_info_text("\n\n")
-        self._append_info_text("Under the hood " + "\n", ("caption", "right"))
+        self._append_info_text(tr("Under the hood") + "\n", ("caption", "right"))
         self._append_info_text(
-            "This dialog uses `pipkin`, a new command line tool for managing "
-            "MicroPython and CircuitPython packages. ",
+            tr("This dialog uses `pipkin`, a new command line tool for managing "
+            "MicroPython and CircuitPython packages."),
             ("right",),
         )
         self._append_info_text("See ", ("right",))
         self._append_info_text("https://pypi.org/project/pipkin/", ("url", "right"))
-        self._append_info_text(" for more info. \n", ("right",))
+        self._append_info_text(" for more info.\n", ("right",))
 
     def _show_read_only_instructions(self):
         self._append_info_text(tr("Not available") + "\n", ("caption",))
