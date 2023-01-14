b68f8e44aae625f547a5ad485cf197d3d643aa0b
Aivar Annamaa <aivar.annamaa@gmail.com>
2023-01-14 17:42:05 +0200

    Fix the name for Slowenian, #2611

--- thonny/languages.py
+++ thonny/languages.py
@@ -27,7 +27,7 @@ LANGUAGES_DICT = {
     "pt_BR": "Português (BR)",
     "ro_RO": "Român",
     "ru_RU": "Русский",
-    "sl_SI": "Slovène",
+    "sl_SI": "Slovenščina",
     "sk_SK": "Slovenčina [BETA]",
     "fi_FI": "Suomi [BETA]",
     "sv_SE": "Svenska [BETA]",
