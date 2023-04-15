d704dfe69974eb878735eeb2fac17b1152af6889
Aivar Annamaa <aivar.annamaa@gmail.com>
2023-04-15 11:38:32 +0300

    Fix regressions caused by #2616

--- thonny/plugins/circuitpython/cirpy_back.py
+++ thonny/plugins/circuitpython/cirpy_back.py
@@ -1,6 +1,13 @@
 import os.path
+import sys
 from logging import getLogger
 from typing import List, Optional
+
+# make sure thonny folder is in sys.path (relevant in dev)
+thonny_container = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+if thonny_container not in sys.path:
+    sys.path.insert(0, thonny_container)
+
 
 from thonny.plugins.micropython.bare_metal_backend import (
     BareMetalMicroPythonBackend,
