eafe7c869e2a610f8e1167d942089ba24fe31d2b
Aivar Annamaa <aivar.annamaa@gmail.com>
2023-04-22 11:15:40 +0300

    Update create_release.sh

--- packaging/mac/create_release.sh
+++ packaging/mac/create_release.sh
@@ -8,7 +8,9 @@ echo "Releasing $thonny_version"
 echo 
 echo "### Creating regular installer ###############################################"
 ./prepare_dist_bundle.sh $thonny_version ../requirements-regular-bundle.txt
+echo "--- Signing -------------------------------------"
 ./sign_bundle_in_build.sh
+echo "--- Creating installer -------------------------------------"
 ./create_installer_from_build.sh "thonny" "Thonny" $thonny_version
 
 echo 
