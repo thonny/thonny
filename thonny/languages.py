31a4f593a6c22f6678421d8f17f7f38e2fcd86ad
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2023-09-28 00:52:18 +0300

    Update translations

--- thonny/languages.py
+++ thonny/languages.py
@@ -10,6 +10,7 @@ BASE_LANGUAGE_NAME = "English (US) [default]"
 # https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 
 LANGUAGES_DICT = {
+    "ca_ES": "Català",
     "cs_CZ": "Čeština [BETA]",
     "de_DE": "Deutsch",
     "et_EE": "Eesti",
