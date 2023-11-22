397679cd8f7eb11b20ba9075c694337f5aace54b
Philip Howard <github@gadgetoid.com>
2023-11-22 12:12:08 +0000

    cpython_frontend: Support for ~/.virtualenvs directory on unix. (#2978)
    * cpython_frontend: Support for ~/.virtualenvs directory on unix.
    
    Add a simple glob expansion of the "~/.virtualenvs" directory to find
    python binaries managed by virtualenvwrapper.
    
    This mirrors VSCode's Python extension, which scans this directory for
    interpreters.
    
    Signed-off-by: Phil Howard <phil@gadgetoid.com>
    
    * Consult WORKON_HOME variable
    
    ---------
    
    Signed-off-by: Phil Howard <phil@gadgetoid.com>
    Co-authored-by: Aivar Annamaa <aivarannamaa@users.noreply.github.com>

--- thonny/plugins/cpython_frontend/cp_front.py
+++ thonny/plugins/cpython_frontend/cp_front.py
@@ -1,3 +1,4 @@
+import glob
 import os.path
 import subprocess
 import sys
@@ -412,6 +413,11 @@ def _get_interpreters():
             os.path.expanduser("~/.local/bin"),
             os.path.expanduser("~/anaconda3/bin"),
         ]
+
+        workon_home = os.environ.get("WORKON_HOME", os.path.expanduser("~/.virtualenvs"))
+        virtualenvwrapper = os.path.join(workon_home, "*/bin")
+        dirs += glob.glob(virtualenvwrapper)
+
         for dir_ in dirs:
             # if the dir_ is just a link to another dir_, skip it
             # (not to show items twice)
