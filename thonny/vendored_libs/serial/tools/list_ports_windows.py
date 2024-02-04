893ed8225790bc6d32a8db5294d0db374aec1e5e
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2024-02-04 23:44:40 +0200

    Simplify list_ports_windows

--- thonny/vendored_libs/serial/tools/list_ports_windows.py
+++ thonny/vendored_libs/serial/tools/list_ports_windows.py
@@ -42,6 +42,7 @@ LPBYTE = PBYTE = ctypes.c_void_p        # XXX avoids error about types
 ACCESS_MASK = DWORD
 REGSAM = ACCESS_MASK
 
+
 class GUID(ctypes.Structure):
     _fields_ = [
         ('Data1', DWORD),
@@ -59,19 +60,6 @@ class GUID(ctypes.Structure):
             ''.join(["{:02x}".format(d) for d in self.Data4[2:]]),
         )
 
-
-DEVPROPID = ULONG
-DEVPROPGUID = GUID
-DEVPROPTYPE = ULONG
-PDEVPROPTYPE = ctypes.POINTER(DEVPROPTYPE)
-
-class DEVPROPKEY(ctypes.Structure):
-    _fields_ = [
-        ('fmtid', DEVPROPGUID),
-        ('pid', DEVPROPID),
-    ]
-
-PDEVPROPKEY = ctypes.POINTER(DEVPROPKEY)
 
 class SP_DEVINFO_DATA(ctypes.Structure):
     _fields_ = [
@@ -227,10 +215,6 @@ SetupDiGetClassDevs = setupapi.SetupDiGetClassDevsW
 SetupDiGetClassDevs.argtypes = [ctypes.POINTER(GUID), PCTSTR, HWND, DWORD]
 SetupDiGetClassDevs.restype = HDEVINFO
 SetupDiGetClassDevs.errcheck = ValidHandle
-
-SetupDiGetDeviceProperty = setupapi.SetupDiGetDevicePropertyW
-SetupDiGetDeviceProperty.argtypes = [HDEVINFO, PSP_DEVINFO_DATA, PDEVPROPKEY, PDEVPROPTYPE, PBYTE, DWORD, PDWORD, DWORD]
-SetupDiGetDeviceProperty.restype = BOOL
 
 SetupDiGetDeviceRegistryProperty = setupapi.SetupDiGetDeviceRegistryPropertyW
 SetupDiGetDeviceRegistryProperty.argtypes = [HDEVINFO, PSP_DEVINFO_DATA, DWORD, PDWORD, PBYTE, DWORD, PDWORD]
@@ -862,27 +846,7 @@ def iterate_comports(enable_iocontrol=True):
                 # Just like the previous version.
                 get_usb_info_from_device_property(g_hdi, devinfo, szHardwareID_str, info)
 
-            # interface
-            devproptype = DEVPROPTYPE()
-            szInterface = ctypes.create_unicode_buffer(250)
-            if SetupDiGetDeviceProperty(
-                    g_hdi,
-                    ctypes.byref(devinfo),
-                    ctypes.byref(DEVPROPKEY(
-                        DEVPROPGUID(0x540B947E, 0x8B40, 0x45BC, (0xA8, 0xA2, 0x6A, 0x0B, 0x89, 0x4C, 0xBD, 0xA2)),
-                        4)
-                    ),
-                    ctypes.byref(devproptype),
-                    ctypes.byref(szInterface),
-                    ctypes.sizeof(szInterface) - 1,
-                    None,
-                    0):
-                info.interface = szInterface.value
-            else:
-                info.interface = None
-
             yield info
-
         SetupDiDestroyDeviceInfoList(g_hdi)
 
 
