9b71e970f1950cfa96288f7541b8360d25cd18c9
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2024-02-04 17:11:07 +0200

    Revert "Add Interface patch to Product patch, #3100"
    This reverts commit 72cc263585521b3d3caba6437136920f155c4ccd.

--- thonny/plugins/micropython/mp_front.py
+++ thonny/plugins/micropython/mp_front.py
@@ -1096,18 +1096,11 @@ def _list_serial_ports_uncached(skip_logging: bool = False):
             os.path.islink = lambda _: False
 
         if sys.platform == "win32":
-            from serial import win32
-
-            if hasattr(win32, "DeviceIoControl"):
-                # this is patched version
+            try:
+                from adafruit_board_toolkit._list_ports_windows import comports
+            except ImportError:
+                logger.info("Falling back to serial.tools.list_ports.comports")
                 from serial.tools.list_ports import comports
-            else:
-                try:
-                    logger.debug("Falling back to adafruit_board_toolkit")
-                    from adafruit_board_toolkit._list_ports_windows import comports
-                except ImportError:
-                    logger.debug("Falling back to unpatched serial.tools.list_ports.comports")
-                    from serial.tools.list_ports import comports
         elif sys.platform == "darwin":
             try:
                 from adafruit_board_toolkit._list_ports_osx import comports
