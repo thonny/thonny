dc9d4f27db417ef03308d4261b88371c0a522b85
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2024-02-05 10:42:03 +0200

    Fix import order for microbit and calliope back-ends

--- thonny/plugins/calliope/calliope_back.py
+++ thonny/plugins/calliope/calliope_back.py
@@ -3,14 +3,13 @@ import sys
 from logging import getLogger
 from typing import List, Optional
 
-from thonny.plugins.simplified_micropython.simplified_mp_back import SimplifiedMicroPythonBackend
-
 # make sure thonny folder is in sys.path (relevant in dev)
 thonny_container = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 if thonny_container not in sys.path:
     sys.path.insert(0, thonny_container)
 
 from thonny.plugins.micropython.bare_metal_backend import launch_bare_metal_backend
+from thonny.plugins.simplified_micropython.simplified_mp_back import SimplifiedMicroPythonBackend
 
 # Can't use __name__, because it will be "__main__"
 logger = getLogger("thonny.plugins.calliope.calliope_back")
