32bfac3ccfaaff324999f233ec7147b78af83df8
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2024-09-08 08:55:20 +0300

    Use binary exacutable for launching Thonny in macOS app bundle.
    Allows getting rid of postinstall script, makes the bundle relocatable, prepares way for publishing dmg instead of pkg, #3283, #2296

---
+++ packaging/mac/Thonny.app.initial_template/Contents/Resources/launch.py
@@ -0,0 +1,22 @@
+# inspired by IDLE.app/Contents/IDLE
+
+import sys
+import os.path
+
+execdir = os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), "MacOS")
+executable = os.path.join(execdir, "Python")
+resdir = os.path.join(os.path.dirname(execdir), "Resources")
+libdir = os.path.join(os.path.dirname(execdir), "Frameworks/Python.framework/Versions/3.12/lib")
+ssl_cert_dir = libdir + "/python3.12/site-packages/certifi"
+mainprogram = os.path.join(resdir, "thonnymain.py")
+
+sys.argv.insert(1, mainprogram)
+sys.argv.insert(1, "-I")
+
+os.environ["PYTHON_SYS_EXECUTABLE"] = executable
+os.environ["SSL_CERT_DIR"] = ssl_cert_dir
+os.environ["SSL_CERT_FILE"] = ssl_cert_dir + "/cacert.pem"
+os.environ["TCL_LIBRARY"] = libdir + "/tcl8.6"
+os.environ["TK_LIBRARY"] = libdir + "/tk8.6"
+
+os.execve(executable, sys.argv, os.environ)
---
+++ packaging/mac/launcher/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+swiftc -o ../Thonny.app.initial_template/Contents/MacOS/thonny launcher.swift
