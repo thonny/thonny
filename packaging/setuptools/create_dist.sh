77f7aa60dde126a2760e764c9070e84bd170493a
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2025-02-24 19:04:04 +0200

    Update create_dist.sh

--- packaging/setuptools/create_dist.sh
+++ packaging/setuptools/create_dist.sh
@@ -1,5 +1,6 @@
 cd ../..
 rm -rf build
+rm -rf thonny.egg-info
 python3 setup.py bdist_wheel -d packaging/setuptools
 python3 setup.py sdist --formats=gztar -d packaging/setuptools
 cd packaging/setuptools
