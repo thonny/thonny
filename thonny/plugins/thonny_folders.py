599a0086ed92af596e6851e7fa485ede1f478a7e
sampo <sampotaas@gnail.com>
2026-02-20 11:56:08 +0100

    added messagebox before opening data filder.

--- thonny/plugins/thonny_folders.py
+++ thonny/plugins/thonny_folders.py
@@ -2,13 +2,21 @@
 """Adds commands for opening certain Thonny folders"""
 
 from thonny import get_thonny_user_dir, get_workbench
+from tkinter.messagebox import askyesno
 from thonny.languages import tr
 from thonny.ui_utils import open_path_in_system_file_manager
 
 
 def load_plugin() -> None:
     def cmd_open_data_dir():
+        quit_data_dir = askyesno(
+            title= tr("Thonny Closure"),
+            message= tr("Do you want to close Thonny before enterring the data folder? This is useful if you want to manually edit the configuration files. "),
+            parent= get_workbench()
+        )
         open_path_in_system_file_manager(get_thonny_user_dir())
+        if quit_data_dir:
+            get_workbench().quit()
 
     def cmd_open_program_dir():
         open_path_in_system_file_manager(get_workbench().get_package_dir())
